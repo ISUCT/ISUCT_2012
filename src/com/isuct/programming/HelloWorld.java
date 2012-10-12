acaceb3acb7a6253dae264b079a1ae1fb11ec9d8
jskonst <jskonst@yandex.ru>
2012-10-12 17:34:28 +0400

    Changed name of package, .gitignore fix for Netbeans

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -1,8 +1,9 @@
-package com.isuct.programming.group47;
+package com.isuct.programming;
 
 public class HelloWorld {
 
 	public static void main(String [] args){
 		System.out.println("Hello world 2");
+                System.out.println("worked in netbeans too");
 	}
 }
