05b1a422d8da1d3f97d4a95fb1ca974ad022ffef
jskonst <jskonst@yandex.ru>
2012-10-12 16:49:03 +0400

    Testing from another place

--- src/com/isuct/programming/group47/HelloWorld.java
+++ src/com/isuct/programming/group47/HelloWorld.java
@@ -3,6 +3,6 @@ package com.isuct.programming.group47;
 public class HelloWorld {
 
 	public static void main(String [] args){
-		System.out.println("Hello world");
+		System.out.println("Hello world 2");
 	}
 }
