df2f0b9fd7edf6778a2efebb1e9b72694db07712
Stud_17 <Stud_17@17_11.efdnew.isuct.ru>
2012-10-17 16:09:29 +0400

    Лабароторная работа

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -16,32 +16,46 @@ public class HelloWorld {
 	 */
 	public static void main(String [] args){
 		//Объявляем и инициализируем переменные
-		float x = 1.25f; 
-		float a = 1.1f;
-		float b = 2.0f;
+                float x = 2.4f; 
+		float a = 7.2f;
+		float b = 4.2f;
 		//вычисляем числитель Math.pow(число, степень)
-		double chisl = Math.pow(x, 2) + Math.pow((x+b), (1/3f));
+		double chisl = Math.abs( a - b*x);
 		//вычисляем знаменатель Логарифм числа по основанию = (натуральный логарифм числа)/ (натуральный логарифм основания)
-		double znamen = Math.log(a+x)/Math.log(2);
+		double znamen = Math.pow(Math.log10(x),3);
 		//вычисляем y
-		double y = chisl/znamen;
+		double y = Math.pow (chisl/znamen, 1/2f);
 		//Выводим результат
 		System.out.println("Икс равен = "+x+" Игрек равен = "+y);
 		
 		//повторяем вычисления для второго значения х, но следует помнить. что переменные мы уже объявляли
 		//повторно их объявлять не надо, а надо только присвоить новые значени		
 		
-		x=2.5f;
+		x=2.8f;
 		chisl = Math.pow(x, 2) + Math.pow((x+b), (1/3f));
 		znamen = Math.log(a+x)/Math.log(2);
-		y = chisl/znamen;
+		y = Math.pow (chisl/znamen, 1/2f);
 		System.out.println("X = "+x+" Y = "+y);
 		
 		//И для третьего значения так же и т.д (про циклы мы пока ничего не знаем)
-		x=3.7f;
+		x=3.9f;
 		chisl = Math.pow(x, 2) + Math.pow((x+b), (1/3f));
 		znamen = Math.log(a+x)/Math.log(2);
-		y = chisl/znamen;
+		y = Math.pow (chisl/znamen, 1/2f);
+		System.out.println("Дано = "+x+" Посчитано = "+y);
+                
+                x=4.7f;
+		chisl = Math.pow(x, 2) + Math.pow((x+b), (1/3f));
+		znamen = Math.log(a+x)/Math.log(2);
+		y = Math.pow (chisl/znamen, 1/2f);
+		System.out.println("Дано = "+x+" Посчитано = "+y);
+                
+                x=3.16f;
+		chisl = Math.pow(x, 2) + Math.pow((x+b), (1/3f));
+		znamen = Math.log(a+x)/Math.log(2);
+		y = Math.pow (chisl/znamen, 1/2f);
 		System.out.println("Дано = "+x+" Посчитано = "+y);
 	}
 }
+	
+		
