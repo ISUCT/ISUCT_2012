5681cf69fa00fd6be9abc8d2e435526d7f86881d
Stud_17 <Stud_17@17_03.efdnew.isuct.ru>
2012-10-17 16:38:32 +0400

    Лаб. раб. №1

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -2,46 +2,51 @@ package com.isuct.programming;
 
 public class HelloWorld {
 
-	/**
-	 * Здесь мы просто посчитаем
-	 * @param args
-	 * пример:
-	 *     2
-	 *    x + кубический корень из (x+b)
-	 * y=-------------------------------
-	 *    log(a+x)по основанию 2
-	 *    x=(поочередно несколько значений) 1.25; 2.5; 3.7
-	 *    a=1.1
-	 *    b=2
-	 */
 	public static void main(String [] args){
-		//Объявляем и инициализируем переменные
-		float x = 1.25f; 
+
+		float x = 1.21f; 
 		float a = 1.1f;
-		float b = 2.0f;
-		//вычисляем числитель Math.pow(число, степень)
-		double chisl = Math.pow(x, 2) + Math.pow((x+b), (1/3f));
-		//вычисляем знаменатель Логарифм числа по основанию = (натуральный логарифм числа)/ (натуральный логарифм основания)
-		double znamen = Math.log(a+x)/Math.log(2);
-		//вычисляем y
+		float b = 0.09f;
+		
+		double chisl = Math.log10((Math.pow(x, 2)-1));
+		
+		double znamen = Math.log((a*(Math.pow(x,2)-b)))/Math.log(5);
+		
 		double y = chisl/znamen;
-		//Выводим результат
-		System.out.println("Икс равен = "+x+" Игрек равен = "+y);
 		
-		//повторяем вычисления для второго значения х, но следует помнить. что переменные мы уже объявляли
-		//повторно их объявлять не надо, а надо только присвоить новые значени		
+		System.out.println("x= "+x+" y= "+y);
 		
-		x=2.5f;
-		chisl = Math.pow(x, 2) + Math.pow((x+b), (1/3f));
-		znamen = Math.log(a+x)/Math.log(2);
+		
+		x=1.76f;
+	 chisl = Math.log10((Math.pow(x, 2)-1));
+		
+		znamen = Math.log((a*(Math.pow(x,2)-b)))/Math.log(5);
+	
 		y = chisl/znamen;
 		System.out.println("X = "+x+" Y = "+y);
 		
-		//И для третьего значения так же и т.д (про циклы мы пока ничего не знаем)
-		x=3.7f;
-		chisl = Math.pow(x, 2) + Math.pow((x+b), (1/3f));
-		znamen = Math.log(a+x)/Math.log(2);
+                x=2.53f
+		chisl = Math.log10((Math.pow(x, 2)-1));
+		
+		 znamen = Math.log((a*(Math.pow(x,2)-b)))/Math.log(5);
+		
+	 y = chisl/znamen;
+		System.out.println("x= "+x+" y= "+y);
+                
+                x=3.48f;
+	 chisl = Math.log10((Math.pow(x, 2)-1));
+		
+		znamen = Math.log((a*(Math.pow(x,2)-b)))/Math.log(5);
+	
 		y = chisl/znamen;
-		System.out.println("Дано = "+x+" Посчитано = "+y);
+		System.out.println("X = "+x+" Y = "+y);
+                
+                 x=4.52f;
+	 chisl = Math.log10((Math.pow(x, 2)-1));
+		
+		znamen = Math.log((a*(Math.pow(x,2)-b)))/Math.log(5);
+	
+		y = chisl/znamen;
+		System.out.println("X = "+x+" Y = "+y);
 	}
 }
