1dd27ed32e15489d13c225bdf107d3a67e0e0450
jskonst <jskonst@yandex.ru>
2012-10-31 13:24:01 +0400

    Пример второй лабораторной

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -3,45 +3,58 @@ package com.isuct.programming;
 public class HelloWorld {
 
 	/**
-	 * Здесь мы просто посчитаем
+	 * Теперь посчитаем с циклом 
 	 * @param args
 	 * пример:
+	 * xn=1
+	 * dx=0.2
+	 * xk=10
+	 * если x>=5
 	 *     2
 	 *    x + кубический корень из (x+b)
 	 * y=-------------------------------
 	 *    log(a+x)по основанию 2
-	 *    x=(поочередно несколько значений) 1.25; 2.5; 3.7
+	 *    
+	 * если x<5
+	 * 
+	 *     3
+	 *    x + a + b
+	 * y=-------------------------------
+	 *         2
+	 *    sin(x  )
+	 *    
 	 *    a=1.1
 	 *    b=2
 	 */
 	public static void main(String [] args){
 		//Объявляем и инициализируем переменные
-		float x = 1.25f; 
-		float a = 1.1f;
-		float b = 2.0f;
-		//вычисляем числитель Math.pow(число, степень)
-		double chisl = Math.pow(x, 2) + Math.pow((x+b), (1/3f));
-		//вычисляем знаменатель Логарифм числа по основанию = (натуральный логарифм числа)/ (натуральный логарифм основания)
-		double znamen = Math.log(a+x)/Math.log(2);
-		//вычисляем y
-		double y = chisl/znamen;
+		double xn = 1f;
+		double dx=0.2;
+		double xk=10;
+		double a = 1.1f;
+		double b = 2.0f;
+		double chisl=0;
+		double znamen=0;
+		double y=0;
+		for(double x=xn;x<=xk;x+=dx){
+		    if(x<5){
+			//вычисляем числитель Math.pow(число, степень)
+			chisl = Math.pow(x, 3) + a + b;
+			//вычисляем знаменатель Логарифм числа по основанию = (натуральный логарифм числа)/ (натуральный логарифм основания)
+			znamen = Math.sin(Math.pow(x,2));
+			//вычисляем y
+			y= chisl/znamen;
+		    }else{
+			//вычисляем числитель Math.pow(число, степень)
+			chisl = Math.pow(x, 2) + Math.pow((x+b), (1/3f));
+			//вычисляем знаменатель Логарифм числа по основанию = (натуральный логарифм числа)/ (натуральный логарифм основания)
+			znamen = Math.log(a+x)/Math.log(2);
+			//вычисляем y
+			y= chisl/znamen;
+		    }
 		//Выводим результат
 		System.out.println("Икс равен = "+x+" Игрек равен = "+y);
 		
-		//повторяем вычисления для второго значения х, но следует помнить. что переменные мы уже объявляли
-		//повторно их объявлять не надо, а надо только присвоить новые значени		
-		
-		x=2.5f;
-		chisl = Math.pow(x, 2) + Math.pow((x+b), (1/3f));
-		znamen = Math.log(a+x)/Math.log(2);
-		y = chisl/znamen;
-		System.out.println("X = "+x+" Y = "+y);
-		
-		//И для третьего значения так же и т.д (про циклы мы пока ничего не знаем)
-		x=3.7f;
-		chisl = Math.pow(x, 2) + Math.pow((x+b), (1/3f));
-		znamen = Math.log(a+x)/Math.log(2);
-		y = chisl/znamen;
-		System.out.println("Дано = "+x+" Посчитано = "+y);
-	}
+		}
+		}
 }
