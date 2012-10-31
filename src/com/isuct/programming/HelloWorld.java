254350351dcdcb1a7cf45428b34d6d9183c27d5a
1 <1>
2012-10-31 15:30:52 +0300

    Лабораторная №2

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -16,44 +16,46 @@ public class HelloWorld {
 	 */
 	public static void main(String [] args){
 		//Объявляем и инициализируем переменные
-		float x = 4.48f; 
-		float a = 0.4f;
-		float b = 0.8f;
-		//вычисляем числитель Math.pow(число, степень)
-		double chisl = (Math.pow(a, x) - Math.pow(b, x)) * Math.pow ( a*b,(1/3f));
-		//вычисляем знаменатель Логарифм числа по основанию = (натуральный логарифм числа)/ (натуральный логарифм основания)
-		double znamen = Math.log10 (a/b);
-		//вычисляем y
-		double y = chisl/znamen;
-		//Выводим результат
-		System.out.println("Икс равен = "+x+" Игрек равен = "+y);
+		          float x = 4.48f;
+            float a = 0.4f;
+            float b = 0.8f;
+            for (double Xn = 3.2; Xn <= 6.2; Xn += 0.6) {
+                //вычисляем числитель Math.pow(число, степень)
+                double chisl = (Math.pow(a, Xn) - Math.pow(b, Xn)) * Math.pow(a * b, (1 / 3f));
+                //вычисляем знаменатель Логарифм числа по основанию = (натуральный логарифм числа)/ (натуральный логарифм основания)
+                double znamen = Math.log10(a / b);
+                //вычисляем y
+                double y = chisl / znamen;
+                //Выводим результат
+                System.out.println("Икс равен = " + Xn + " Игрек равен = " + y);
+            }
 		
 		//повторяем вычисления для второго значения х, но следует помнить. что переменные мы уже объявляли
 		//повторно их объявлять не надо, а надо только присвоить новые значени		
 		
-		x=3.56f;
-		chisl = (Math.pow(a, x) - Math.pow(b, x)) * Math.pow ( a*b,(1/3f));
-		znamen = Math.log10 (a/b);
-		y = chisl/znamen;
-		System.out.println("X = "+x+" Y = "+y);
+		//x=3.56f;
+		//chisl = (Math.pow(a, x) - Math.pow(b, x)) * Math.pow ( a*b,(1/3f));
+		//znamen = Math.log10 (a/b);
+		//y = chisl/znamen;
+		//System.out.println("X = "+x+" Y = "+y);
 		
 		//И для третьего значения так же и т.д (про циклы мы пока ничего не знаем)
-		x=2.78f;
-		chisl = (Math.pow(a, x) - Math.pow(b, x)) * Math.pow ( a*b,(1/3f));
-		znamen = Math.log10 (a/b);
-		y = chisl/znamen;
-		System.out.println("Дано = "+x+" Посчитано = "+y);
+		//x=2.78f;
+		//chisl = (Math.pow(a, x) - Math.pow(b, x)) * Math.pow ( a*b,(1/3f));
+		//znamen = Math.log10 (a/b);
+		//y = chisl/znamen;
+		//System.out.println("Дано = "+x+" Посчитано = "+y);
                 
-                x=5.28f;
-		chisl = (Math.pow(a, x) - Math.pow(b, x)) * Math.pow ( a*b,(1/3f));
-		znamen = Math.log10 (a/b);
-		y = chisl/znamen;
-		System.out.println("X = "+x+" Y = "+y);
+                //x=5.28f;
+		//chisl = (Math.pow(a, x) - Math.pow(b, x)) * Math.pow ( a*b,(1/3f));
+		//znamen = Math.log10 (a/b);
+		//y = chisl/znamen;
+		//System.out.println("X = "+x+" Y = "+y);
                 
-                x=3.21f;
-		chisl = (Math.pow(a, x) - Math.pow(b, x)) * Math.pow ( a*b,(1/3f));
-		znamen = Math.log10 (a/b);
-		y = chisl/znamen;
-		System.out.println("X = "+x+" Y = "+y);
+                //x=3.21f;
+		//chisl = (Math.pow(a, x) - Math.pow(b, x)) * Math.pow ( a*b,(1/3f));
+		//znamen = Math.log10 (a/b);
+		//y = chisl/znamen;
+		//System.out.println("X = "+x+" Y = "+y);
 	}
 }
