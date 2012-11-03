23fd54f24d33a5d67f43ff1c029205d6581c6fee
Stud_5 <Stud_5@5_14.efdnew.isuct.ru>
2012-11-03 10:33:49 +0400

    lab2

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -2,59 +2,15 @@ package com.isuct.programming;
 
 public class HelloWorld {
 
-	/**
-	 * Теперь посчитаем с циклом 
-	 * @param args
-	 * пример:
-	 * xn=1
-	 * dx=0.2
-	 * xk=10
-	 * если x>=5
-	 *     2
-	 *    x + кубический корень из (x+b)
-	 * y=-------------------------------
-	 *    log(a+x)по основанию 2
-	 *    
-	 * если x<5
-	 * 
-	 *     3
-	 *    x + a + b
-	 * y=-------------------------------
-	 *         2
-	 *    sin(x  )
-	 *    
-	 *    a=1.1
-	 *    b=2
-	 */
 	public static void main(String [] args){
-		//Объявляем и инициализируем переменные
-		double xn = 1f;
-		double dx=0.2;
-		double xk=10;
-		double a = 1.1f;
-		double b = 2.0f;
-		double chisl=0;
-		double znamen=0;
-		double y=0;
-		for(double x=xn;x<=xk;x+=dx){
-		    if(x<5){
-			//вычисляем числитель Math.pow(число, степень)
-			chisl = Math.pow(x, 3) + a + b;
-			//вычисляем знаменатель Логарифм числа по основанию = (натуральный логарифм числа)/ (натуральный логарифм основания)
-			znamen = Math.sin(Math.pow(x,2));
-			//вычисляем y
-			y= chisl/znamen;
-		    }else{
-			//вычисляем числитель Math.pow(число, степень)
-			chisl = Math.pow(x, 2) + Math.pow((x+b), (1/3f));
-			//вычисляем знаменатель Логарифм числа по основанию = (натуральный логарифм числа)/ (натуральный логарифм основания)
-			znamen = Math.log(a+x)/Math.log(2);
-			//вычисляем y
-			y= chisl/znamen;
-		    }
-		//Выводим результат
-		System.out.println("Икс равен = "+x+" Игрек равен = "+y);
+            double xn = 0.11f;
+            double dx=0.05;
+            double xk=0.36;
+            double y=0;
+		for(double x=0.11;x<=0.36;x=(x+0.05)){
+                 y=(Math.pow(Math.sin(x),3)+(Math.pow(Math.cos(x),3))*Math.log10(x));
+                    System.out.println("x=" + x + "y=" + y);
 		
 		}
-		}
+	}
 }
