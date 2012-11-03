3b372a31dc3e7703398e6661aedf3868056fa233
Stud_5 <Stud_5@5_08.efdnew.isuct.ru>
2012-11-03 11:13:02 +0400

    Разрешены конфликты

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -10,59 +10,40 @@ package com.isuct.programming;
  */
 public class HelloWorld{
 
-	/**
-	 * Теперь посчитаем с циклом 
-	 * @param args
-	 * пример:
-	 * xn=1
-	 * dx=0.2
-	 * xk=10
-	 * если x>=5
-	 *     2
-	 *    x + кубический корень из (x+b)
-	 * y=-------------------------------
-	 *    log(a+x)по основанию 2
-	 *    
-	 * если x<5
-	 * 
-	 *     3
-	 *    x + a + b
-	 * y=-------------------------------
-	 *         2
-	 *    sin(x  )
-	 *    
-	 *    a=1.1
-	 *    b=2
-	 */
-	public static void main(String [] args){
-		//Объявляем и инициализируем переменные
-		double xn = 1f;
-		double dx=0.2;
-		double xk=10;
-		double a = 1.1f;
-		double b = 2.0f;
-		double chisl=0;
-		double znamen=0;
+	 /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+                double xn = 3.5f;
+		double dx=0.6;
+		double xk=6.5;
+		double a = -2.5f;
+		double b = 3.4f;
+		double Chisl=0;
+		double Znamen=0;
 		double y=0;
-		for(double x=xn;x<=xk;x+=dx){
-		    if(x<5){
-			//вычисляем числитель Math.pow(число, степень)
-			chisl = Math.pow(x, 3) + a + b;
-			//вычисляем знаменатель Логарифм числа по основанию = (натуральный логарифм числа)/ (натуральный логарифм основания)
-			znamen = Math.sin(Math.pow(x,2));
-			//вычисляем y
-			y= chisl/znamen;
-		    }else{
-			//вычисляем числитель Math.pow(число, степень)
-			chisl = Math.pow(x, 2) + Math.pow((x+b), (1/3f));
-			//вычисляем знаменатель Логарифм числа по основанию = (натуральный логарифм числа)/ (натуральный логарифм основания)
-			znamen = Math.log(a+x)/Math.log(2);
-			//вычисляем y
-			y= chisl/znamen;
-		    }
-		//Выводим результат
-		System.out.println("Икс равен = "+x+" Игрек равен = "+y);
-		
-		}
-		}
+       
+                   /*Для первого уравнения системы.*/ 
+        
+        for(double x=3.5; x<=6.5; x=(x + 0.6)){  
+            if(x<5){
+            
+               Chisl = Math.pow(Math.log10(x), 2)*((Math.pow(a, 2))+x);
+               Znamen = Math.pow((x+a), 2);
+               y = Chisl / Znamen;
+        
+        
+                  /*Для второго уравнения системы.*/
+       }else{
+               Chisl=Math.pow((a+(b*x)), 2.5);
+               Znamen=1.8+ Math.pow((Math.cos(a*x)),3);
+               y = Chisl / Znamen;
+              
+          }    
+        System.out.println("x=" + x + "y=" + y);
+    
+    }
 }
+   }
