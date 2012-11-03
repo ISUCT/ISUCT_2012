4d295c6cd46fdea17d4083ffb65dab25e4f81ff3
Stud_5 <Stud_5@5_08.efdnew.isuct.ru>
2012-11-03 11:09:50 +0400

    Разрешены конфликты

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -16,34 +16,16 @@ public class HelloWorld{
     public static void main(String[] args) {
         // TODO code application logic here
        
-                double xn = 3.5f;
-		double dx=0.6;
-		double xk=6.5;
-		double a = -2.5f;
-		double b = 3.4f;
-		double Chisl=0;
-		double Znamen=0;
-		double y=0;
-       
-                   /*Для первого уравнения системы.*/ 
-        
-        for(double x=3.5; x<=6.5; x=(x + 0.6)){  
-            if(x<5){
-            
-               Chisl = Math.pow(Math.log10(x), 2)*((Math.pow(a, 2))+x);
-               Znamen = Math.pow((x+a), 2);
-               y = Chisl / Znamen;
-        
-        
-                  /*Для второго уравнения системы.*/
-       }else{
-               Chisl=Math.pow((a+(b*x)), 2.5);
-               Znamen=1.8+ Math.pow((Math.cos(a*x)),3);
-               y = Chisl / Znamen;
-              
-          }    
-        System.out.println("x=" + x + "y=" + y);
-    
-    }
-}
-}
+             double a = 2.0f;
+             double b = 0.95f;
+       for (double x=1.25;x<2.75;x=(x+0.3)){
+            double chisl=1+ Math.pow(Math.log10(x/a),2);
+            double znamen=b-Math.pow(Math.E,x/a);
+            double y=chisl/znamen;
+       System.out.println("x="+x+" y="+y);
+       }
+       } 
+        }
+
+
+
