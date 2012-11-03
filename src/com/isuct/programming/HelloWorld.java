0e60acf45f8662014fc97edaef738cd6ddc36d21
Stud_5 <Stud_5@5_12.efdnew.isuct.ru>
2012-11-03 11:27:11 +0400

    вторая работа

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -2,21 +2,26 @@ package com.isuct.programming;
 
 public class HelloWorld {
 
-/**
-* @param args
-*/
-public static void main(String[] args) {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
 
-float a = 1.35f;
-float b = 0.98f;
+        float a = 1.35f;
+        float b = 0.98f;
 
-float [] x = {0.35f, 1.28f, 3.51f, 5.21f, 4.16f};
-float y;
+        float Xn = 1.14f;
+        float Xk = 4.24f;
+        float dX = 0.62f;
 
-for (int i = 0; i < 5; i++) {
-y = (float) ((float) Math.pow(a * x[i] + b, 1.f/3.f) / (float) Math.pow(Math.log10(x[i]), 2));
-System.out.println("y(x[" + (i + 1) + "]) = " + y);
-} 
-}
+        float currX = Xn;
+        float y;
 
+        while (currX <= Xk) {
+            y = (float) ((float) Math.pow(a * currX + b, 1.f / 3.f) / (float) Math.pow(Math.log10(currX), 2));
+            System.out.println("y(" + currX + ") = " + y);
+
+            currX += dX;
+        }
+    }
 }
