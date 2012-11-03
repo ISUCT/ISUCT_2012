7696fa4bfe7bc0fd5e0460b6b5ace4755c579268
Stud_5 <Stud_5@5_09.efdnew.isuct.ru>
2012-11-03 11:06:35 +0400

    Разрешены конфликты

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -10,84 +10,27 @@ package com.isuct.programming;
  */
 public class HelloWorld {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+  public static void main(String[] args) {
         // TODO code application logic here
-        double b = 1.1;
- double Cvadrat1 = Math.pow(b, 2);
- double x1 = 0.1;
- double Cvadrat2 = Math.pow(x1, 2);
- double Raznost1 = Cvadrat1 - Cvadrat2;
- double Modul1 = Math.abs(Raznost1);
- double a = 2.0;
- double log = Math.log(Modul1) / Math.log(a);
- double Cvadrat3 = Math.pow(a, 2);
- double Raznost2 = Cvadrat2 - Cvadrat3;
- double Modul2=Math.abs(Raznost2);
- double Znamen = Math.pow(Modul2, 1 / 5f);
- double y = (log / Znamen);
- System.out.println ("y="+y);
-
-
-
- double Cvadrat11 = Math.pow(b, 2);
- double x2 = 0.3;
- double Cvadrat21 = Math.pow(x2, 2);
- double Raznost11 = Cvadrat11 - Cvadrat21;
- double Modul11 = Math.abs(Raznost11);
- double log1 = Math.log(Modul11) / Math.log(a);
- double Cvadrat31 = Math.pow(a, 2);
- double Raznost21 = Cvadrat21 - Cvadrat31;
- double Modul21=Math.abs(Raznost21);
- double Znamen1 = Math.pow(Modul21, 1 / 5f);
- double y1 = (log1 / Znamen1);
- System.out.println ("y1="+y1);
-
-
-
- double Cvadrat112 = Math.pow(b, 2);
- double x3 = 0.4;
- double Cvadrat212 = Math.pow(x3, 2);
- double Raznost112 = Cvadrat112 - Cvadrat212;
- double Modul112 = Math.abs(Raznost112);
- double log12 = Math.log(Modul112) / Math.log(a);
- double Cvadrat312 = Math.pow(a, 2);
- double Raznost212 = Cvadrat212 - Cvadrat312;
- double Modul212=Math.abs(Raznost212);
- double Znamen12 = Math.pow(Modul212, 1 / 5f);
- double y12 = (log12 / Znamen12);
- System.out.println ("y12="+y12);
-
-
- double Cvadrat1121 = Math.pow(b, 2);
- double x4 = 0.45;
- double Cvadrat2121 = Math.pow(x4, 2);
- double Raznost1121 = Cvadrat1121 - Cvadrat2121;
- double Modul1121 = Math.abs(Raznost1121);
- double log121 = Math.log(Modul1121) / Math.log(a);
- double Cvadrat3121 = Math.pow(a, 2);
- double Raznost2121 = Cvadrat2121 - Cvadrat3121;
- double Modul2121=Math.abs(Raznost2121);
- double Znamen121 = Math.pow(Modul2121, 1 / 5f);
- double y121 = (log121 / Znamen121);
- System.out.println ("y121="+y121);
-
-
-
- double Cvadrat11213 = Math.pow(b, 2);
- double x5 = 0.65;
- double Cvadrat21213 = Math.pow(x5, 2);
- double Raznost11213 = Cvadrat11213 - Cvadrat21213;
- double Modul11213 = Math.abs(Raznost11213);
- double log1213 = Math.log(Modul11213) / Math.log(a);
- double Cvadrat31213 = Math.pow(a, 2);
- double Raznost21213 = Cvadrat21213 - Cvadrat31213;
- double Modul21213=Math.abs(Raznost21213);
- double Znamen1213 = Math.pow(Modul21213, 1 / 5f);
- double y1213 = (log1213 / Znamen1213);
- System.out.println ("y1213="+y1213);
- 
+        
+		
+        float a = 2.0f;		
+        float b = 1.1f;		
+     		
+        for(double x=0.08;x<=1.08;x=(x+0.2)){		  
+             double chisl=(Math.log(Math.abs(Math.pow(b,2)-Math.pow(x, 2))) / Math.log(a))*Math.abs(Math.pow(b, 2)-Math.pow(x, 2));
+             double znamen=Math.pow((Math.abs(Math.pow(x,2)-Math.pow(a,2))), 1 / 5f);
+             double y=chisl/znamen;
+                System.out.println("x="+x+"y="+y);
+                
+                
+                
+                
+            }
+        
+            
+        
+        
+        
     }
 }
