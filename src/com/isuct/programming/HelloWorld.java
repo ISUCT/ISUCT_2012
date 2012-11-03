cc8bb4141069d86ab76f4e65c299afa900f11de5
Stud_5 <Stud_5@5_08.efdnew.isuct.ru>
2012-11-03 11:04:16 +0400

    Разрешены конфликты

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -10,50 +10,40 @@ package com.isuct.programming;
  */
 public class HelloWorld{
 
-    /**
+     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+       
+                double xn = 3.5f;
+		double dx=0.6;
+		double xk=6.5;
+		double a = -2.5f;
+		double b = 3.4f;
+		double Chisl=0;
+		double Znamen=0;
+		double y=0;
+       
+                   /*Для первого уравнения системы.*/ 
+        
+        for(double x=3.5; x<=6.5; x=(x + 0.6)){  
+            if(x<5){
+            
+               Chisl = Math.pow(Math.log10(x), 2)*((Math.pow(a, 2))+x);
+               Znamen = Math.pow((x+a), 2);
+               y = Chisl / Znamen;
         
         
-    float a=2.0f;
-    float b=0.95f;
-    float x=2.2f;
-    double chisl;
-    chisl=1+Math.log10(x/a);
-    double znamen;
-    znamen=b-Math.pow(Math.E,x/a);
-    double y=chisl/znamen;
-    System.out.println("x="+x+"y="+y);
-   
-    x=3.78f;
-    chisl=1+Math.log10(x/a);
-    znamen=b-Math.pow(Math.E,x/a);
-    y=chisl/znamen;
-    System.out.println("x="+x+"y="+y);
+                  /*Для второго уравнения системы.*/
+       }else{
+               Chisl=Math.pow((a+(b*x)), 2.5);
+               Znamen=1.8+ Math.pow((Math.cos(a*x)),3);
+               y = Chisl / Znamen;
+              
+          }    
+        System.out.println("x=" + x + "y=" + y);
     
-    x=4.51f;
-    chisl=1+Math.log10(x/a);
-    znamen=b-Math.pow(Math.E,x/a);
-    y=chisl/znamen;
-    System.out.println("x="+x+"y="+y);
-    
-    
-    x=6.58f;
-    chisl=1+Math.log10(x/a);
-    znamen=b-Math.pow(Math.E,x/a);
-    y=chisl/znamen;
-    System.out.println("x="+x+"y="+y);
-    
-    x=1.2f;
-    chisl=1+Math.log10(x/a);
-    znamen=b-Math.pow(Math.E,x/a);
-    y=chisl/znamen;
-    System.out.println("x="+x+"y="+y);
-    
-
-
-
     }
+}
 }
