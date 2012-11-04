dc5e0bcef875d5cf653ccef79e39e27f287a897e
Nastya Lobanova <naastya95@gmail.com>
2012-11-04 10:55:43 +0600

    Более новую версию забрали

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -6,27 +6,27 @@ public static void main(String [] args){
 float x = 1.28f;
 float a = 1.6f;
 double y= (Math.pow ((a),(x+1)*(x-1))- (Math.log((x-1)*(x+1))/Math.log(10)) + Math.pow (((x-1)*(x+1)),(1/3)));
-System.out.println("x=");
+System.out.println("x"+x+"y"+y);
 
 x = 1.36f;
 a = 1.6f;
 y= (Math.pow ((a),(x+1)*(x-1))- (Math.log((x-1)*(x+1))/Math.log(10)) + Math.pow (((x-1)*(x+1)),(1/3)));
-System.out.println("x=");
+System.out.println("x"+x+"y"+y);
 
 x = 2.47f;
 a = 1.6f;
 y= (Math.pow ((a),(x+1)*(x-1))- (Math.log((x-1)*(x+1))/Math.log(10)) + Math.pow (((x-1)*(x+1)),(1/3)));
-System.out.println("x=");
+System.out.println("x"+x+"y"+y);
 
 x = 3.68f;
 a = 1.6f;
 y= (Math.pow ((a),(x+1)*(x-1))- (Math.log((x-1)*(x+1))/Math.log(10)) + Math.pow (((x-1)*(x+1)),(1/3)));
-System.out.println("x=");
+System.out.println("x"+x+"y"+y);
 
 x = 4.56f;
 a = 1.6f;
 y= (Math.pow ((a),(x+1)*(x-1))- (Math.log((x-1)*(x+1))/Math.log(10)) + Math.pow (((x-1)*(x+1)),(1/3)));
-System.out.println("x=");
+System.out.println("x"+x+"y"+y);
 
 
 
