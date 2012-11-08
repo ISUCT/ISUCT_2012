ae6866146d5f33b138541c5cc1a258a4d477032f
Stud_6 <Stud_6@6_07.efdnew.isuct.ru>
2012-11-08 18:05:03 +0400

    вторая лабораторная

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -1,44 +1,14 @@
 package com.isuct.programming;
-
 public class HelloWorld {
-
-	public static void main(String[] args){
-float x = 4.48f;
+public static void main(String[] args){
 float a = 0.4f;
 float b = 0.8f;
-double chisl;
-chisl = Math.pow((a),(x))- Math.pow ((b),(x));
-double znamen;
-znamen = Math.log(a/b)/ Math.log(10);
-double y = chisl/znamen * Math.pow ((a*b),(1/3));
-System.out.println("x="+x+"y="+y);
-
-x = 3.56f;
-chisl = Math.pow((a),(x))- Math.pow ((b),(x));
-znamen = Math.log(a/b)/ Math.log(10);
-y = chisl/znamen * Math.pow ((a*b),(1/3));
-System.out.println("x="+x+"y="+y);
-
-
-
-x = 2.78f;
-chisl = Math.pow((a),(x))- Math.pow ((b),(x));
-znamen = Math.log(a/b)/ Math.log(10);
-y = chisl/znamen * Math.pow ((a*b),(1/3));
-System.out.println("x="+x+"y="+y);
-
-x = 5.28f;
-chisl = Math.pow((a),(x))- Math.pow ((b),(x));
-znamen = Math.log(a/b)/ Math.log(10);
-y = chisl/znamen * Math.pow ((a*b),(1/3));
-System.out.println("x="+x+"y="+y);
-
-x = 3.21f;
-chisl = Math.pow((a),(x))- Math.pow ((b),(x));
-znamen = Math.log(a/b)/ Math.log(10);
-y = chisl/znamen * Math.pow ((a*b),(1/3));
-System.out.println("x="+x+"y="+y);
-
+for (double x=3.2; x<=6.2; x+=0.6){
+double chisl=Math.pow(a,x)-Math.pow(b,x)*Math.pow(a*b,1/3);
+double znamen=Math.log10(a/b);
+double y=chisl/znamen;
+System.out.println("x"+x+"y"+y);
 }
 
+}
 }
