5027d1ff036d8da5a4c867142de80cf4075b75ed
jskonst <jskonst@yandex.ru>
2012-11-09 23:24:06 +0400

    Не были  проиндексированы измененные файлы, поэтому изменения и не заливались

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -6,29 +6,31 @@ public static void main(String [] args){
 float x = 1.28f;
 float a = 1.6f;
 double y= (Math.pow ((a),(x+1)*(x-1))- (Math.log((x-1)*(x+1))/Math.log(10)) + Math.pow (((x-1)*(x+1)),(1/3)));
-System.out.println("x"+x+"y"+y);
+System.out.println("x"+x +"y"+y);
 
 x = 1.36f;
 a = 1.6f;
 y= (Math.pow ((a),(x+1)*(x-1))- (Math.log((x-1)*(x+1))/Math.log(10)) + Math.pow (((x-1)*(x+1)),(1/3)));
-System.out.println("x"+x+"y"+y);
+System.out.println("x"+x +"y"+y);
 
 x = 2.47f;
 a = 1.6f;
 y= (Math.pow ((a),(x+1)*(x-1))- (Math.log((x-1)*(x+1))/Math.log(10)) + Math.pow (((x-1)*(x+1)),(1/3)));
-System.out.println("x"+x+"y"+y);
+System.out.println("x"+x +"y"+y);
 
 x = 3.68f;
 a = 1.6f;
 y= (Math.pow ((a),(x+1)*(x-1))- (Math.log((x-1)*(x+1))/Math.log(10)) + Math.pow (((x-1)*(x+1)),(1/3)));
-System.out.println("x"+x+"y"+y);
+System.out.println("x"+x +"y"+y);
 
 x = 4.56f;
 a = 1.6f;
 y= (Math.pow ((a),(x+1)*(x-1))- (Math.log((x-1)*(x+1))/Math.log(10)) + Math.pow (((x-1)*(x+1)),(1/3)));
-System.out.println("x"+x+"y"+y);
+System.out.println("x"+x +"y"+y);
 
-
-
+for (x = 1.2f; x <= 3.7; x += 0.5) {
+y= (Math.pow ((a),(x+1)*(x-1))- (Math.log((x-1)*(x+1))/Math.log(10)) + Math.pow (((x-1)*(x+1)),(1/3)));
+System.out.println("Ответ(2)x"+x +"y"+y);
 }
-        }
+}
+}
