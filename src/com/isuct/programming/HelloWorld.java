34664bc653fdb1c2bab9c04b05874389d00a1993
Алена <@>
2012-11-18 14:30:56 +0400

    Лабораторная работа №3. Вариант 22.

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -1,29 +1,20 @@
 package com.isuct.programming;
-
-    public class HelloWorld {    
-    public static void main(String [] args){  
-        
-        float x = 1.31f; 
-	float a = 2.25f;
-        double y = Math.pow(a, (x*x-1))-Math.log(x*x-1)/Math.log(10)+Math.pow((x*x-1), (1/3f));
-        System.out.println("x="+x+"  y="+y);
+public class HelloWorld {
     
-        x=1.39f;
-        y = Math.pow(a, (x*x-1))-Math.log(x*x-1)/Math.log(10)+Math.pow((x*x-1), (1/3f));
-        System.out.println("x="+x+"  y="+y);
-    
-        x=1.44f;
-        y = Math.pow(a, (x*x-1))-Math.log(x*x-1)/Math.log(10)+Math.pow((x*x-1), (1/3f));      
-        System.out.println("x="+x+"  y="+y);
-    
-        x=1.56f;
-        y = Math.pow(a, (x*x-1))-Math.log(x*x-1)/Math.log(10)+Math.pow((x*x-1), (1/3f));
-        System.out.println("x="+x+"  y="+y);
-    
-        x=1.92f;
-        y = Math.pow(a, (x*x-1))-Math.log(x*x-1)/Math.log(10)+Math.pow((x*x-1), (1/3f));
-        System.out.println("x="+x+"  y="+y);
-        
-    }
-    
+        public static void main(String[] args) {                     
+            
+            Calc defaultConstr = new Calc();
+            
+            System.out.println("------------Решаем задачу В------------");
+            defaultConstr.taskB();
+            
+            System.out.println("------------Решаем задачу A —----------");
+            defaultConstr.taskA(2.25, 1.2, 0.3, 2.7);
+            
+            System.out.println("------------Решаем задачу A с перегруженным конструктором------------");
+            Calc nDefConstr = new Calc(2.25, 1.2, 0.3, 2.7);
+            nDefConstr.taskA();
+        }
 }
+
+ 
