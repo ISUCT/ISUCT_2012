65696d1bebb4eb07e060c8cf852b2b0b32eba455
2 <2>
2012-11-20 17:51:29 +0300

    3-я Лабораторная

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -10,10 +10,10 @@ public class HelloWorld {
 		System.out.println("------------Решаем задачу В------------");
 		defaultConstr.taskB();
 		System.out.println("------------Решаем задачу A ------------");
-		defaultConstr.taskA(1.1,2.0,1,0.2,5);
+		defaultConstr.taskA(4.48,3.56,2.78,5.28,3.21);
 		//Решим задачу А - с использованием перегруженного конструктора
 		System.out.println("------------Решаем задачу A с перегруженным конструктором------------");
-		Calc nDefConstr = new Calc(1.1,2.0,1,0.5,7);
+		Calc nDefConstr = new Calc(4.48,3.56,2.78,5.28,3.21);
 		//тогда мы можем вызвать метод без передачи дополнительных параметров
 		nDefConstr.taskA();
 		}
