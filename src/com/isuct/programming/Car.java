c2018b696638ce93ec97a629ad6cfac26386efe9
jskonst <jskonst@yandex.ru>
2012-11-22 15:11:03 +0400

    Пример творческого задания. Понятно что пример можно было бы расширить, но для понимания и начала - выбран вот такой минимум

---
+++ src/com/isuct/programming/Car.java
@@ -0,0 +1,144 @@
+package com.isuct.programming;
+
+/**
+ * Класс (фабрика, новый тип данных) по производству автомобилей
+ * с закрытыми полями доступа
+ * 
+ * @author jskonst
+ *
+ */
+public class Car {
+	/**
+	 * Закрытые члены класса
+	 */
+	//Ширина, длина и высота машины
+	private int length;
+	private int height;
+	private int width;
+	
+	
+	//координаты машины
+	private int x; //X
+	private int y; //Y
+	
+	
+	//Параметры
+	private int numOfWheels;
+	private int numOfDors;
+	private int numOfSeets;
+	
+	//Параметры расхода	
+	private float fuelLevel;
+	private float rashod;
+	
+	/**
+	 * Конструктор по умолчанию - создает "седаны"
+	 * @param length длина
+	 * @param height высота
+	 * @param width ширина
+	 */
+	public Car(int length, int height, int width){
+		//Присваиваем переданные нам значения - закрытым членам класса
+		//При этом я не проверяю правильность вводе - а надо бы
+		this.length = length; 
+		this.height = height;
+		this.width = width;
+		//И параметры типовой машины
+		this.numOfDors = 4;
+		this.numOfSeets = 5;
+		this.numOfWheels = 4;
+	}
+	
+	/**
+	 * А вот тут мы можем произвести любую машину
+	 * @param length длина
+	 * @param height высота
+	 * @param width ширина
+	 * @param numOfWheels Число колес
+	 * @param numOfDors число дверей
+	 * @param numOfSeets число сидений
+	 */
+	public Car(int length, int height, int width, int numOfWheels, int numOfDors, int numOfSeets){
+		//Присваиваем переданные нам значения - закрытым членам класса
+		
+		this.length = length;
+		this.height = height;
+		this.width = width;
+		this.numOfDors = numOfDors;
+		this.numOfSeets = numOfSeets;
+		this.numOfWheels = numOfWheels;
+	}
+	
+	//Методы получения значений закрытых параметров класса
+	
+	public int getHeigth(){
+		return this.height;
+	}
+
+	public int getWidth(){
+		return this.width;
+	}
+
+	public int getLength(){
+		return this.length;
+	}
+
+	public int getNumOfDors(){
+		return this.numOfDors;
+	}
+	
+	public int getNumOfSeets(){
+		return this.numOfSeets;
+	}
+	public int getNumOfWheels(){
+		return this.numOfWheels;
+	}
+	
+	public int[] getCoords(){
+		return new int[]{x,y};
+	}
+	
+	public float getFuelLevel(){
+		return this.fuelLevel;
+	}
+	public float getRashod(){
+		return this.rashod;
+	}
+	
+	//Методы - устанавливающие значения
+	public void setRashod(float rashod){
+		this.rashod = rashod;
+	}
+	public void setFuelLevel(float fuel){
+		this.fuelLevel = fuel;
+	}
+	public void setPosition(int x, int y){
+		this.x= x;
+		this.y= y;
+	}
+	
+	/**
+	 * Перемещаем машину, но двигаться мы умеем только ортогонально
+	 * @param axis ось x или y
+	 * @param distance расстояние на оси
+	 * @return истину или ложь - если переместиться не удасться
+	 */
+	 
+	public boolean move(String axis, int distance){
+		//Посчитаем сколько топлива надо
+		float needFuel = distance*rashod/100;
+		if((fuelLevel - needFuel)<0){
+			return false;
+		}else{
+			if(axis.equals("x")){
+				x+=distance;
+			}else if(axis.equals("y")){
+				y+=distance;
+			}else{
+				return false;
+			}
+			fuelLevel-=needFuel;
+			return true;
+		}
+	}
+}
--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -1,20 +1,50 @@
 package com.isuct.programming;
 
+import com.isuct.programming.Car;
+
 public class HelloWorld {
 
-
-	public static void main(String [] args){
-		//Решим задачи А и задачу B с использованием внешнего класса Calc
-		//Сперва используем конструктор по умолчанию
-		Calc defaultConstr = new Calc();
-		System.out.println("------------Решаем задачу В------------");
-		defaultConstr.taskB();
-		System.out.println("------------Решаем задачу A ------------");
-		defaultConstr.taskA(1.1,2.0,1,0.2,5);
-		//Решим задачу А - с использованием перегруженного конструктора
-		System.out.println("------------Решаем задачу A с перегруженным конструктором------------");
-		Calc nDefConstr = new Calc(1.1,2.0,1,0.5,7);
-		//тогда мы можем вызвать метод без передачи дополнительных параметров
-		nDefConstr.taskA();
+	public static void main(String[] args) {
+		// создаем классику
+		Car vaz2107 = new Car(3000, 1400, 1800);
+		vaz2107.setPosition(0, 0);
+		vaz2107.setFuelLevel(20);
+		vaz2107.setRashod(8);
+		if (vaz2107.move("x", 100)) {
+			System.out.println("2107 Новые координаты теперь х= "
+					+ vaz2107.getCoords()[0] + " y= " + vaz2107.getCoords()[1]);
+		} else {
+			System.out.println("2107 Не хватит топлива");
 		}
+
+		if (vaz2107.move("y", 500)) {
+			System.out.println("2107 Новые координаты теперь х= "
+					+ vaz2107.getCoords()[0] + " y= " + vaz2107.getCoords()[1]);
+		} else {
+			System.out.println("2107 Не хватит топлива");
+		}
+		System.out.println("2107 Проверим координаты х= " + vaz2107.getCoords()[0]
+				+ " y= " + vaz2107.getCoords()[1]);
+
+		
+		Car ladaLargus= new Car(3000, 1400, 1800,4,5,5);
+		ladaLargus.setPosition(0, 0);
+		ladaLargus.setFuelLevel(30);
+		ladaLargus.setRashod(5);
+		if (vaz2107.move("y", 100)) {
+			System.out.println("Largus Новые координаты теперь х= "
+					+ vaz2107.getCoords()[0] + " y= " + vaz2107.getCoords()[1]);
+		} else {
+			System.out.println("Largus Не хватит топлива");
+		}
+
+		if (vaz2107.move("y", 100)) {
+			System.out.println("Largus Новые координаты теперь х= "
+					+ vaz2107.getCoords()[0] + " y= " + vaz2107.getCoords()[1]);
+		} else {
+			System.out.println("Largus Не хватит топлива");
+		}
+
+		
+	}
 }
