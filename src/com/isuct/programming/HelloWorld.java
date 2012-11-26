6036d46962e653fcbd4a649bf3ef316c6a40a285
Admin <Admin@microsof-2bf323>
2012-11-27 02:00:48 +0300

    код

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -3,24 +3,43 @@ package com.isuct.programming;
 public class HelloWorld {
 
 
-	public static void main(String [] args){
-        double xn = 0.11f;
-        double xk = 0.36;
-        double dx = 0.05;
-        double y = 0;
-        for (double x = 0.11; x <= 0.36; x = (x + 0.05)) {
-            y = (Math.pow(Math.sin(x),3)+(Math.pow(Math.cos(x),3))*Math.log10(x));
-                    System.out.println("x=" + x + "y=" + y);
+public static double func(double x, double a, double b) 
+{
+return ((Math.pow((x - a), (double)2 / (double)3)) + (Math.pow(Math.abs(x + b), (double)1 / (double)5))) / 
+(Math.pow((Math.pow(x, 2) - Math.pow((a + b), 2)), (double)1 / (double)9));
+}
 
-            Calc defaultConstr = new Calc(0.2, 0.3, 0.38, 0.43, 0.57);
-            System.out.println("------------Решаем задачу В------------");
-            defaultConstr.taskB(0.2,0.3);
-            System.out.println("------------Решаем задачу A —----------");
-            defaultConstr.taskA(0.2, 0.3, 0.38, 0.43, 0.57);
-            System.out.println("------------Решаем задачу A с перегруженным конструктором------------");
-            Calc nDefConstr = new Calc(0.2, 0.3, 0.38, 0.43, 0.57);
-            nDefConstr.taskA();
+public static void main(String[] args) {
+// TODO Auto-generated method stub
 
-        }
-    }
+double a = 0.8;
+double b = 0.4;
+double Xn = 1.23;
+double Xk = 7.23;
+double dX = 1.2;
+
+double[] x = new double[5];
+
+x[0] = 1.88;
+x[1] = 2.26;
+x[2] = 3.84;
+x[3] = 4.55;
+x[4] = -6.21;
+
+System.out.println("zadacha A");
+
+while (Xn <= Xk)
+{
+System.out.println("y[" + Xn + "] = " + func(Xn, a, b));
+Xn += dX; 
+}
+
+System.out.println("zadacha B");
+
+for (int i = 0; i < 5; i++) {
+System.out.println("y[" + i + "] = " + func(x[i], a, b));
+}
+
+}
+
 }
