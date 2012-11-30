299c4f54658753fa54defccafb458796d692899c
админ <@10.139.126.47>
2012-12-01 02:05:02 +0400

    gg

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -1,50 +1,50 @@
 package com.isuct.programming;
 
-import com.isuct.programming.Car;
+import com.isuct.programming.cat;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// создаем классику
-		Car vaz2107 = new Car(3000, 1400, 1800);
-		vaz2107.setPosition(0, 0);
-		vaz2107.setFuelLevel(20);
-		vaz2107.setRashod(8);
-		if (vaz2107.move("x", 100)) {
-			System.out.println("2107 Новые координаты теперь х= "
-					+ vaz2107.getCoords()[0] + " y= " + vaz2107.getCoords()[1]);
-		} else {
-			System.out.println("2107 Не хватит топлива");
-		}
+// создаем кошку Мусю
+cat Musya = new cat(100, 20, 4, 500, 3);
+Musya.setPosition(0, 0);
+Musya.setSleepLevel(10);
+Musya.setRashod(7);
+if (Musya.move("x", 80)) {
+System.out.println("Musya Новые координаты теперь х= "
++ Musya.getCoords()[0] + " y= " + Musya.getCoords()[1]);
+} else {
+System.out.println("Musya Не хватит уровня сна");
+}
 
-		if (vaz2107.move("y", 500)) {
-			System.out.println("2107 Новые координаты теперь х= "
-					+ vaz2107.getCoords()[0] + " y= " + vaz2107.getCoords()[1]);
-		} else {
-			System.out.println("2107 Не хватит топлива");
-		}
-		System.out.println("2107 Проверим координаты х= " + vaz2107.getCoords()[0]
-				+ " y= " + vaz2107.getCoords()[1]);
+if (Musya.move("y", 200)) {
+System.out.println("Musya Новые координаты теперь х= "
++ Musya.getCoords()[0] + " y= " + Musya.getCoords()[1]);
+} else {
+System.out.println("Musya Не хватит уровня еды");
+}
+System.out.println("Musya Проверим координаты х= "
++ Musya.getCoords()[0] + " y= " +
+Musya.getCoords()[1]);
 
-		
-		Car ladaLargus= new Car(3000, 1400, 1800,4,5,5);
-		ladaLargus.setPosition(0, 0);
-		ladaLargus.setFuelLevel(30);
-		ladaLargus.setRashod(5);
-		if (vaz2107.move("y", 100)) {
-			System.out.println("Largus Новые координаты теперь х= "
-					+ vaz2107.getCoords()[0] + " y= " + vaz2107.getCoords()[1]);
-		} else {
-			System.out.println("Largus Не хватит топлива");
-		}
+// создаем собаку Чапа
+cat Mars = new cat(130, 15, 4, 400, 2);
+Mars.setPosition(0, 0);
+Mars.setSleepLevel(20);
+Mars.setRashod(9);
 
-		if (vaz2107.move("y", 100)) {
-			System.out.println("Largus Новые координаты теперь х= "
-					+ vaz2107.getCoords()[0] + " y= " + vaz2107.getCoords()[1]);
-		} else {
-			System.out.println("Largus Не хватит топлива");
-		}
+if (Mars.move("y", 90)) {
+System.out.println("Mars Новые координаты теперь х= "
++Mars.getCoords()[0] + " y= " + Mars.getCoords()[1]);
+} else {
+System.out.println("Mars Не хватит уровня сна");
+}
 
-		
-	}
+if (Mars.move("y", 90)) {
+System.out.println("Mars Новые координаты теперь х= "
++ Mars.getCoords()[0] + " y= " + Mars.getCoords()[1]);
+} else {
+System.out.println("Mars Не хватит уровня сна");
+}
+}
 }
---
+++ src/com/isuct/programming/cat.java
@@ -0,0 +1,114 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author админ
+ */
+public class cat {
+    
+private float sleepLevel;
+private float rashod;
+private int dlina;
+private int chislOnog;
+private int ves;
+private int colour;
+private int age;
+private int x;
+private int y;
+
+	/**
+* А вот тут мы можем произвести любую кошку
+*
+* @param ves
+* вес
+* @param chislOnog
+* Число лап
+* @param colour
+* цвет
+* @param age
+* возраст
+* @param sleepLevel
+* уровень сна
+* @param rashod
+* расход сна
+* @param dlina
+* длина
+*/
+
+public cat(int dlina,int ves,int chislOnog, int colour, int age) {
+this.dlina = dlina;
+this.ves = ves;
+this.chislOnog = chislOnog;
+this.colour = colour;
+this.age = age;
+
+                }
+
+public int getDlina(){
+return this.dlina;
+}	public int getVes(){
+return this.ves;
+}
+
+public int getColour(){
+return this.colour;
+}
+
+public int getAge(){
+return this.age;
+}
+
+public int getChislOnog(){
+return this.chislOnog;
+}
+
+
+public int[] getCoords() {
+return new int[] { x, y };
+}
+
+public float getFuelLevel() {
+return this.sleepLevel;
+}
+
+public float getRashod() {
+return this.rashod;
+}
+
+public void setRashod(float rashod) {
+this.rashod = rashod;
+}
+
+public void setSleepLevel(float fuel) {
+this.sleepLevel = fuel;
+}
+
+public void setPosition(int x, int y) {
+this.x = x;
+this.y = y;
+}
+
+public boolean move(String axis, int distance) {
+
+float needFuel = distance * rashod / 100;
+if ((sleepLevel - needFuel) < 0) {
+return false;
+} else {
+if (axis.equals("x")) {
+x += distance;
+} else if (axis.equals("y")) {
+y += distance;
+} else {
+return false;
+}
+sleepLevel -= needFuel;
+return true;
+}
+}
+
+
+}
+
