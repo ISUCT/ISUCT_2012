b52689b3ca922d8bca0d8496995c37648f3317d4
Admin <Admin@microsof-2bf323>
2012-12-01 00:12:55 +0300

    lala

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -2,25 +2,48 @@ package com.isuct.programming;
 
 public class HelloWorld {
 
+public static void main(String[] args) {
+		// создаем собаку Барон
+		dog Baron = new dog(100, 20, 4, 500, 3);
+		Baron.setPosition(0, 0);
+		Baron.setSleepLevel(10);
+		Baron.setRashod(7);
+		if (Baron.move("x", 80)) {
+			System.out.println("Baron Новые координаты теперь х= "
+					+ Baron.getCoords()[0] + " y= " + Baron.getCoords()[1]);
+		} else {
+			System.out.println("Baron Не хватит уровня сна");
+		}
 
-	public static void main(String [] args){
-        double xn = 0.11f;
-        double xk = 0.36;
-        double dx = 0.05;
-        double y = 0;
-        for (double x = 0.11; x <= 0.36; x = (x + 0.05)) {
-            y = (Math.pow(Math.sin(x),3)+(Math.pow(Math.cos(x),3))*Math.log10(x));
-                    System.out.println("x=" + x + "y=" + y);
+		if (Baron.move("y", 200)) {
+			System.out.println("Baron Новые координаты теперь х= "
+					+ Baron.getCoords()[0] + " y= " + Baron.getCoords()[1]);
+		} else {
+			System.out.println("Baron Не хватит уровня еды");
+		}
+		System.out.println("Dak Проверим координаты х= "
+				+ Baron.getCoords()[0] + " y= " + 
+Baron.getCoords()[1]);
 
-            Calc defaultConstr = new Calc(0.2, 0.3, 0.38, 0.43, 0.57);
-            System.out.println("------------Решаем задачу В------------");
-            defaultConstr.taskB(0.2,0.3);
-            System.out.println("------------Решаем задачу A —----------");
-            defaultConstr.taskA(0.2, 0.3, 0.38, 0.43, 0.57);
-            System.out.println("------------Решаем задачу A с перегруженным конструктором------------");
-            Calc nDefConstr = new Calc(0.2, 0.3, 0.38, 0.43, 0.57);
-            nDefConstr.taskA();
+		// создаем собаку Чапа
+		dog Chapa = new dog(130, 15, 4, 400, 2);
+		Chapa.setPosition(0, 0);
+		Chapa.setSleepLevel(20);
+		Chapa.setRashod(9);
 
-        }
-    }
+		if (Chapa.move("y", 90)) {
+			System.out.println("Chapa Новые координаты теперь х= "
+					+Chapa.getCoords()[0] + " y= " + Chapa.getCoords()[1]);
+		} else {
+			System.out.println("Chapa Не хватит уровня сна");
+		}
+
+		if (Chapa.move("y", 90)) {
+			System.out.println("Chapa Новые координаты теперь х= "
+					+ Chapa.getCoords()[0] + " y= " + Chapa.getCoords()[1]);
+		} else {
+			System.out.println("Chapa Не хватит уровня сна");
+		}
+	}
+
 }
---
+++ src/com/isuct/programming/dog.java
@@ -0,0 +1,112 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class dog {
+    
+    private float sleepLevel;
+        private float rashod;
+	private int dlina;
+	private int chislOnog;
+	private int ves;
+	private int colour;
+	private int age;
+	private int x;
+	private int y;
+
+	/**
+	 * А вот тут мы можем произвести любую собаку
+	 *
+	 * @param ves
+	 *            вес
+	 * @param chislOnog
+	 *            Число ног
+	 * @param colour
+	 *            цвет
+	 * @param age
+	 *            возраст
+	 * @param sleepLevel
+	 *            уровень сна
+         * @param rashod
+         *            расход сна
+         * @param dlina
+	 *            длина
+	 */
+        public dog(int dlina,int ves,int chislOnog, int colour, int age) {
+		this.dlina = dlina;
+		this.ves = ves;
+		this.chislOnog = chislOnog;
+		this.colour = colour;
+		this.age = age;
+		
+                }
+
+	public int getDlina(){
+		return this.dlina;
+	}
+
+	public int getVes(){
+		return this.ves;
+	}
+	
+	public int getColour(){
+		return this.colour;
+	}
+
+	public int getAge(){
+		return this.age;
+	}
+
+	public int getChislOnog(){
+		return this.chislOnog;
+	}
+
+
+	public int[] getCoords() {
+		return new int[] { x, y };
+	}
+
+	public float getFuelLevel() {
+		return this.sleepLevel;
+	}
+
+	public float getRashod() {
+		return this.rashod;
+	}
+
+	public void setRashod(float rashod) {
+		this.rashod = rashod;
+	}
+
+	public void setSleepLevel(float fuel) {
+		this.sleepLevel = fuel;
+	}
+
+	public void setPosition(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public boolean move(String axis, int distance) {
+
+		float needFuel = distance * rashod / 100;
+		if ((sleepLevel - needFuel) < 0) {
+			return false;
+		} else {
+			if (axis.equals("x")) {
+				x += distance;
+			} else if (axis.equals("y")) {
+				y += distance;
+			} else {
+				return false;
+			}
+			sleepLevel -= needFuel;
+			return true;
+		}
+	}
+}
