fc11bd817f2f643f8e6abb6d722aac33aed6b339
Тимоша <ana_lginka@mail.ru>
2012-12-01 10:16:46 +0400

    дд

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -4,47 +4,49 @@ import com.isuct.programming.Car;
 
 public class HelloWorld {
 
-	public static void main(String[] args) {
-		// создаем классику
-		Car vaz2107 = new Car(3000, 1400, 1800);
-		vaz2107.setPosition(0, 0);
-		vaz2107.setFuelLevel(20);
-		vaz2107.setRashod(8);
-		if (vaz2107.move("x", 100)) {
-			System.out.println("2107 Новые координаты теперь х= "
-					+ vaz2107.getCoords()[0] + " y= " + vaz2107.getCoords()[1]);
-		} else {
-			System.out.println("2107 Не хватит топлива");
-		}
 
-		if (vaz2107.move("y", 500)) {
-			System.out.println("2107 Новые координаты теперь х= "
-					+ vaz2107.getCoords()[0] + " y= " + vaz2107.getCoords()[1]);
-		} else {
-			System.out.println("2107 Не хватит топлива");
-		}
-		System.out.println("2107 Проверим координаты х= " + vaz2107.getCoords()[0]
-				+ " y= " + vaz2107.getCoords()[1]);
-
-		
-		Car ladaLargus= new Car(3000, 1400, 1800,4,5,5);
-		ladaLargus.setPosition(0, 0);
-		ladaLargus.setFuelLevel(30);
-		ladaLargus.setRashod(5);
-		if (vaz2107.move("y", 100)) {
-			System.out.println("Largus Новые координаты теперь х= "
-					+ vaz2107.getCoords()[0] + " y= " + vaz2107.getCoords()[1]);
-		} else {
-			System.out.println("Largus Не хватит топлива");
-		}
-
-		if (vaz2107.move("y", 100)) {
-			System.out.println("Largus Новые координаты теперь х= "
-					+ vaz2107.getCoords()[0] + " y= " + vaz2107.getCoords()[1]);
-		} else {
-			System.out.println("Largus Не хватит топлива");
-		}
-
-		
-	}
+/**
+*param args
+*/
+public static double func(double x, double a, double b) 
+{
+return ((Math.pow((x - a), (double)2 / (double)3)) + (Math.pow(Math.abs(x + b), (double)1 / (double)5))) / 
+(Math.pow((Math.pow(x, 2) - Math.pow((a + b), 2)), (double)1 / (double)9));
 }
+
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+
+double a = 0.8;
+double b = 0.4;
+double Xn = 1.23;
+double Xk = 7.23;
+double dX = 1.2;
+
+
+double[] x = new double[5];
+
+x[0] = 1.88;
+x[1] = 2.26;
+x[2] = 3.84;
+x[3] = 4.55;
+x[4] = -6.21;
+
+System.out.println("zadacha A");
+
+while (Xn <= Xk)
+{
+System.out.println("y[" + Xn + "] = " + func(Xn, a, b));
+Xn += dX; 
+}
+
+System.out.println("zadacha B");
+
+for (int i = 0; i < 5; i++) {
+System.out.println("y[" + i + "] = " + func(x[i], a, b));
+}
+
+
+}
+
+}	
