6f5035bced415ab0fdf0299a36c4ec0c60456b55
Stud_5 <Stud_5@5_11.efdnew.isuct.ru>
2012-12-01 10:16:55 +0300

    третия работа

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -1,44 +1,21 @@
+
 package com.isuct.programming;
 
 public class HelloWorld {
 
-	public static void main(String[] args){
-float x = 4.48f;
-float a = 0.4f;
-float b = 0.8f;
-double chisl;
-chisl = Math.pow((a),(x))- Math.pow ((b),(x));
-double znamen;
-znamen = Math.log(a/b)/ Math.log(10);
-double y = chisl/znamen * Math.pow ((a*b),(1/3));
-System.out.println("x="+x+"y="+y);
 
-x = 3.56f;
-chisl = Math.pow((a),(x))- Math.pow ((b),(x));
-znamen = Math.log(a/b)/ Math.log(10);
-y = chisl/znamen * Math.pow ((a*b),(1/3));
-System.out.println("x="+x+"y="+y);
-
-
-
-x = 2.78f;
-chisl = Math.pow((a),(x))- Math.pow ((b),(x));
-znamen = Math.log(a/b)/ Math.log(10);
-y = chisl/znamen * Math.pow ((a*b),(1/3));
-System.out.println("x="+x+"y="+y);
-
-x = 5.28f;
-chisl = Math.pow((a),(x))- Math.pow ((b),(x));
-znamen = Math.log(a/b)/ Math.log(10);
-y = chisl/znamen * Math.pow ((a*b),(1/3));
-System.out.println("x="+x+"y="+y);
-
-x = 3.21f;
-chisl = Math.pow((a),(x))- Math.pow ((b),(x));
-znamen = Math.log(a/b)/ Math.log(10);
-y = chisl/znamen * Math.pow ((a*b),(1/3));
-System.out.println("x="+x+"y="+y);
-
-}
-
+	public static void main(String [] args){
+		//Решим задачи А и задачу B с использованием внешнего класса Calc
+		//Сперва используем конструктор по умолчанию
+		Calc defaultConstr = new Calc();
+		System.out.println("------------Решаем задачу В------------");
+		defaultConstr.taskB();
+		System.out.println("------------Решаем задачу A ------------");
+		defaultConstr.taskA(4.48,3.56,2.78,5.28,3.21);
+		//Решим задачу А - с использованием перегруженного конструктора
+		System.out.println("------------Решаем задачу A с перегруженным конструктором------------");
+		Calc nDefConstr = new Calc(4.48,3.56,2.78,5.28,3.21);
+		//тогда мы можем вызвать метод без передачи дополнительных параметров
+		nDefConstr.taskA();
+		}
 }
