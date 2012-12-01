b246bbaae01943d40a4565b325794d9e20888c1d
Stud_5 <Stud_5@5_07.efdnew.isuct.ru>
2012-12-01 11:29:31 +0400

    Творческая работа

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -14,14 +14,44 @@ public class HelloWorld{
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-       double a = 2.0f;
-       double b = 0.95f;
-       for (double x=1.25;x<2.75;x=(x+0.3)){
-           double chisl=1+ Math.pow(Math.log10(x/a),2);
-           double znamen=b-Math.pow(Math.E,x/a);
-           double y=chisl/znamen;
-           System.out.println("x="+x+" y="+y);
-       }
-       } 
-   }
+       Mouse SAMSUNG = new Mouse(1,1,5);
+       SAMSUNG.setPosition(0, 0);
+       SAMSUNG.setElektro(15);
+       
+       if (SAMSUNG.move("x", 100)) {
+			System.out.println("SAMSUNG Новые координаты теперь х= "
+					+ SAMSUNG.getCoords()[0] + " y= " + SAMSUNG.getCoords()[1]);
+		} else {
+			System.out.println("SAMSUNG Не хватит энергии");
+		}
+
+		if (SAMSUNG.move("y", 500)) {
+			System.out.println("SAMSUNG Новые координаты теперь х= "
+					+ SAMSUNG.getCoords()[0] + " y= " + SAMSUNG.getCoords()[1]);
+		} else {
+			System.out.println("SAMSUNG Не хватит энергии");
+		}
+		System.out.println("SAMSUNG Проверим координаты х= " + SAMSUNG.getCoords()[0]
+				+ " y= " + SAMSUNG.getCoords()[1]);
+                
+                Mouse TECH = new Mouse(1, 1, 5,2,3,0XFFFF);
+		TECH.setPosition(0, 0);
+		TECH.setElektro(20);	
+		if (SAMSUNG.move("y", 100)) {
+			System.out.println("TECH Новые координаты теперь х= "
+					+ SAMSUNG.getCoords()[0] + " y= " + SAMSUNG.getCoords()[1]);
+		} else {
+			System.out.println("TECH Не хватит энергии");
+		}
+
+		if (SAMSUNG.move("y", 100)) {
+			System.out.println("TECH Новые координаты теперь х= "
+					+ SAMSUNG.getCoords()[0] + " y= " + SAMSUNG.getCoords()[1]);
+		} else {
+			System.out.println("TECH Не хватит энергии");
+		}
+
+        
+    }
+} 
+
