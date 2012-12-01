3eb1e79b3426a587c347b307f3e51282a1bd2534
SAMSUNG <SAMSUNG@Kseniya>
2012-12-01 10:06:11 +0400

    Laboratornaya rabota №3 1ая версия (рабочая)

---
+++ Panzer/src/panzer/HelloPanzer.java
@@ -0,0 +1,13 @@
+            /*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package panzer;
+
+/**
+ *
+ * @author SAMSUNG
+ */
+public class HelloPanzer {
+
+    
