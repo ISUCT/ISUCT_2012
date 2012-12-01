938f478c54dbe52675bf725352cb107ad0821027
Stud_5 <Stud_5@5_12.efdnew.isuct.ru>
2012-12-01 11:57:42 +0400

    Творческое задание

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -1,27 +1,101 @@
-package com.isuct.programming;
-
-public class HelloWorld {
-
-    /**
-     * @param args
-     */
-    public static void main(String[] args) {
-
-        float a = 1.35f;
-        float b = 0.98f;
-
-        float Xn = 1.14f;
-        float Xk = 4.24f;
-        float dX = 0.62f;
-
-        float currX = Xn;
-        float y;
-
-        while (currX <= Xk) {
-            y = (float) ((float) Math.pow(a * currX + b, 1.f / 3.f) / (float) Math.pow(Math.log10(currX), 2));
-            System.out.println("y(" + currX + ") = " + y);
-
-            currX += dX;
+/* 
+ * To change this template, choose Tools | Templates 
+ * and open the template in the editor.
+ */
+/**
+ * 
+ * @author Admin 
+ */
+        public class wolf {
+     private float sleepLevel;
+        private float rashod;
+        private int food;
+        private int dlina;
+        private int chislOnog;	
+        private int ves;	
+        private int colour;	
+        private int age;	
+        private int x;	
+        private int y;
+        /**	 
+         * А вот тут мы можем произвести любого волка	
+         *	 
+         * @param ves	 
+         *            вес
+         * @param chislOnog	
+         *            Число ног	 
+         * @param colour	
+         *            цвет	
+         * @param age	 
+         *            возраст
+         * @param sleepLevel	
+         *            уровень сна         
+         * @param rashod         
+         *            расход сна         
+         * @param dlina	
+         *            длина
+         * @param food
+         *            потребление пищи
+         */
+public wolf(int dlina,int ves,int chislOnog, int colour, int age, int food) {
+this.food = food;
+this.dlina = dlina;
+this.ves = ves;		
+this.chislOnog = chislOnog;		
+this.colour = colour;		
+this.age = age;
+ }
+        public int getDlina(){		
+        return this.dlina;
         }
-    }
-}
+        public int getfood (){
+            return this.food;
+        }
+	public int getVes(){		
+            return this.ves;
+        }	
+        public int getColour(){		
+            return this.colour;
+        }	
+       public int getAge(){		
+           return this.age;
+       }	
+       public int getChislOnog(){		
+           return this.chislOnog;
+       }	
+       public int[] getCoords() {		
+           return new int[] { x, y };
+       }	
+       public float getFuelLevel() {		
+           return this.sleepLevel;
+       }	
+       public float getRashod() {		
+           return this.rashod;
+       }	
+       public void setRashod(float rashod) {		
+           this.rashod = rashod;
+       }	
+       public void setSleepLevel(float fuel) {		
+           this.sleepLevel = fuel;
+       }	
+       public void setPosition(int x, int y) {		
+           this.x = x;
+            this.y = y;
+       }
+       public boolean move(String axis, int distance) {
+           float needFuel = distance * rashod / 100;		
+           if ((sleepLevel - needFuel) < 0) {
+               return false;		
+           } else {
+               if (axis.equals("x")) {				
+                   x += distance;
+               }else if (axis.equals("y")) {				
+                   y += distance;
+                   } else {			
+                   return false;		
+               }			
+               sleepLevel -= needFuel;		
+               return true;		
+           }	
+       }
+} 
