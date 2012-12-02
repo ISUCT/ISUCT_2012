8519f99b8f3b7bc98a5628b19bf91715b8bbf6c0
Kalyagin Ilya <kalyagin.ilya@gmail.com>
2012-12-02 12:30:00 +0400

    Творческое задание(Robot Y155)незаконченное

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -2,16 +2,27 @@ package com.isuct.programming;
 
 public class HelloWorld {
 
-    
     public static void main(String[] args) {
+        Robot Y155 = new Robot(300, 1000, 350, 12, 2, 1);
+        Y155.setPosition(0, 0);
+        Y155.setBattery(10);
+        Y155.setCharge(8);
+        if (Y155.move("x", 450)) {
+            System.out.println("Y155 Новое место х= "
+                    + Y155.getCoordinates()[0] + " y= " + Y155.getCoordinates()[1]);
+        } else {
+            System.out.println("Y155 ");
+        }
 
-        Calc defaultConstr = new Calc(1.2, 0.48, 0.7, 0.3, 2.2);
-        System.out.println("------------Решаем задачу В------------");
-        defaultConstr.taskB(1.2, 0.48, 0.7, 0.3, 2.2);
-        System.out.println("------------Решаем задачу A ------------");
-        defaultConstr.taskA(1.2, 0.48, 0.7, 0.3, 2.2);
-        System.out.println("------------Решаем задачу A с перегруженным конструктором------------");
-        Calc nDefConstr = new Calc(1.2, 0.48, 0.7, 0.3, 2.2);
+        if (Y155.move("y", 200)) {
+            System.out.println("Y155 Переместился в х= "
+                    + Y155.getCoordinates()[0] + " y= " + Y155.getCoordinates()[1]);
+        } else {
+            System.out.println("Y155 Батаря разряжена");
+        }
+        System.out.println("Y155 Проверка х= " + Y155.getCoordinates()[0]
+                + " y= " + Y155.getCoordinates()[1]);
+
 
     }
 }
