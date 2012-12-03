f47dfd213cfdcb2726dd3bd2fda8020a6be50570
Алена <@>
2012-12-03 15:59:46 +0400

    Лабораторная работа №4. Творческое задание. Создание телефонов Nokia.

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -1,22 +1,48 @@
 package com.isuct.programming;
 
 public class HelloWorld {
-    
-    public static void main(String[] args) {                     
-                       
-            Calc defaultConstr = new Calc();
-            
-            System.out.println("------------Решаем задачу В------------");
-            defaultConstr.taskB();
-            
-            System.out.println("------------Решаем задачу A —----------");
-            defaultConstr.taskA(2.25, 1.2, 0.3, 2.7);
-            
-            System.out.println("------------Решаем задачу A с перегруженным конструктором------------");
-            Calc nDefConstr = new Calc(2.25, 1.2, 0.3, 2.7);
-            nDefConstr.taskA();
-                      
-        }      
 
+	public static void main(String[] args) {
+		// создаем классику
+		Mobile nokia1 = new Mobile(1, 5800, 1);
+                nokia1.setPopolneniescheta(100);
+		nokia1.setMoney(-300);
+                
+		if (nokia1.zvonok()) {
+			System.out.println("Nokia1 Недостаточно средств для совершения звонка. Пополните счет." );
+		
+                } else {
+			System.out.println("Nokia1 Идут гудки. Ждите.");
+ 
+	
+                }
+                
+                Mobile nokia2 = new Mobile(2, 8999, 4);
+                nokia2.setPopolneniescheta(100);
+		nokia2.setMoney(54);
+                
+		if (nokia2.zvonok()) {
+			System.out.println("Nokia2 Недостаточно средств для совершения звонка. Пополните счет." );
+		
+                } else {
+			System.out.println("Nokia2 Идут гудки. Ждите.");
+ 
+	
+                }
+                
+                Mobile nokia3 = new Mobile(2, 15000, 8);
+                nokia3.setPopolneniescheta(100);
+		nokia3.setMoney(-100);
+                
+		if (nokia3.zvonok()) {
+			System.out.println("Nokia3 Недостаточно средств для совершения звонка. Пополните счет." );
+		
+                } else {
+			System.out.println("Nokia3 Идут гудки. Ждите.");
+ 
+	
+                }
+        }
+       
 }
-
+	
