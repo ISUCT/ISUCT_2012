7b40e161b0caee9af3ad5c1153cb01e41191591e
Stud_6 <Stud_6@6_13.efdnew.isuct.ru>
2012-12-06 19:18:10 +0400

    лабораторная работа 2

---
+++ rest/src/rest/Rest.java
@@ -0,0 +1,18 @@
+package rest;
+
+public class Rest {
+    public static void main(String[] args) {
+
+float a = 0.1f;
+float b = 0.5f;
+
+float Xn = 0.15f;
+float Xk = 1.37f;
+float dX = 0.25f;
+float y;
+ for(double x=0.15;x<=1.37;x=(x+0.25)){             
+            y=(float) (a+Math.pow(Math.tan(b*x),2)/ b+Math.pow(Math.tan(a*x),2));            
+            System.out.println("x=" + x + "y=" + y);	
+        }	
+    }
+} 
