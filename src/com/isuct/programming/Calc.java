653713f4b4a99db6a47b2c61794efa86e416a5d9
Администратор <@192.168.0.57>
2012-12-06 18:13:13 +0400

    Фиксируем состояние

--- src/com/isuct/programming/Calc.java
+++ src/com/isuct/programming/Calc.java
@@ -1,64 +1,144 @@
 package com.isuct.programming;
 
-public class Calc {
-    
-    private double xn;
-    private double dx;
-    private double xk;
-    private double a;
-    private double b;
-    
-    /**
-    * Конструктор по умолчанию
-    */
-  public Calc(){
-      
-  }
-    public Calc(double aA,double aB,double aXn,double aDx,double aXk){
-    xn = aXn;
-    dx = aDx;
-    xk = aXk;
-    a = aA;
-    b = aB;
-      }
-    
-   public void taskB(){
-    double a = 0.05f;
-    double b = 0.06f;
-    double chisl = 0;
-    double znamen = 0;
-    double y =0;
-    double[]Xarray = new double[]{0.15,0.26,0.37,0.48,0.56};
-    for (double x : Xarray){
-    chisl = Math.acos((x-b)*(x+b));
-    znamen = Math.asin((x-a)*(x+b));
-    System.out.print("Икс равен="+ x +"Игрек равен ="+ y);
-    
-    }
-   }
-   public void taskA() {
-       double chisl = 0;
-       double znamen = 0;
-       double y = 0;
-       for(double x = xn;x<=xk;x +=dx){
-       chisl = Math.acos((x-b)*(x+b));
-       znamen = Math.asin((x-a)*(x+a));
-       System.out.print ("Икс равен=" + x +"Игрек равен="+ y);
-       }
-   }
-   public void taskA(double a,double b,double xn,double dx,double xk){
-       double chisl = 0;
-       double znamen = 0;
-       double y = 0;
-       for(double x = xn;x<=xk;x +=dx){
-       chisl = Math.acos((x-b)*(x+b));
-       znamen = Math.asin((x-a)*(x+a));
-       System.out.print ("Икс равен=" + x +"Игрек равен="+ y);
-       }
-   }
-   
-} 
- 
-    
-    
-    
+/**
+ * Класс (фабрика, новый тип данных) по производству автомобилей
+ * с закрытыми полями доступа
+ * 
+ * @author jskonst
+ *
+ */
+public class Car {
+	/**
+	 * Закрытые члены класса
+	 */
+	//Ширина, длина и высота машины
+	private int length;
+	private int height;
+	private int width;
+	
+	
+	//координаты машины
+	private int x; //X
+	private int y; //Y
+	
+	
+	//Параметры
+	private int numOfWheels;
+	private int numOfDors;
+	private int numOfSeets;
+	
+	//Параметры расхода	
+	private float fuelLevel;
+	private float rashod;
+	
+	/**
+	 * Конструктор по умолчанию - создает "седаны"
+	 * @param length длина
+	 * @param height высота
+	 * @param width ширина
+	 */
+	public Car(int length, int height, int width){
+		//Присваиваем переданные нам значения - закрытым членам класса
+		//При этом я не проверяю правильность вводе - а надо бы
+		this.length = length; 
+		this.height = height;
+		this.width = width;
+		//И параметры типовой машины
+		this.numOfDors = 4;
+		this.numOfSeets = 5;
+		this.numOfWheels = 4;
+	}
+	
+	/**
+	 * А вот тут мы можем произвести любую машину
+	 * @param length длина
+	 * @param height высота
+	 * @param width ширина
+	 * @param numOfWheels Число колес
+	 * @param numOfDors число дверей
+	 * @param numOfSeets число сидений
+	 */
+	public Car(int length, int height, int width, int numOfWheels, int numOfDors, int numOfSeets){
+		//Присваиваем переданные нам значения - закрытым членам класса
+		
+		this.length = length;
+		this.height = height;
+		this.width = width;
+		this.numOfDors = numOfDors;
+		this.numOfSeets = numOfSeets;
+		this.numOfWheels = numOfWheels;
+	}
+	
+	//Методы получения значений закрытых параметров класса
+	
+	public int getHeigth(){
+		return this.height;
+	}
+
+	public int getWidth(){
+		return this.width;
+	}
+
+	public int getLength(){
+		return this.length;
+	}
+
+	public int getNumOfDors(){
+		return this.numOfDors;
+	}
+	
+	public int getNumOfSeets(){
+		return this.numOfSeets;
+	}
+	public int getNumOfWheels(){
+		return this.numOfWheels;
+	}
+	
+	public int[] getCoords(){
+		return new int[]{x,y};
+	}
+	
+	public float getFuelLevel(){
+		return this.fuelLevel;
+	}
+	public float getRashod(){
+		return this.rashod;
+	}
+	
+	//Методы - устанавливающие значения
+	public void setRashod(float rashod){
+		this.rashod = rashod;
+	}
+	public void setFuelLevel(float fuel){
+		this.fuelLevel = fuel;
+	}
+	public void setPosition(int x, int y){
+		this.x= x;
+		this.y= y;
+	}
+	
+	/**
+	 * Перемещаем машину, но двигаться мы умеем только ортогонально
+	 * @param axis ось x или y
+	 * @param distance расстояние на оси
+	 * @return истину или ложь - если переместиться не удасться
+	 */
+	 
+	public boolean move(String axis, int distance){
+		//Посчитаем сколько топлива надо
+		float needFuel = distance*rashod/100;
+		if((fuelLevel - needFuel)<0){
+			return false;
+		}else{
+			if(axis.equals("x")){
+				x+=distance;
+			}else if(axis.equals("y")){
+				y+=distance;
+			}else{
+				return false;
+			}
+			fuelLevel-=needFuel;
+			return true;
+		}
+	}
+}
--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -1,60 +1,47 @@
 package com.isuct.programming;
 
-public class HelloWorld {
+public class Traktor {
 
-	public static void main(String [] args){
-double a =0.05;
-double b =0.06f;
+	public static void main(String[] args) {
+		Car Traktor = new Car(8000, 2400, 5800);
+		Traktor.setPosition(0, 0);
+		Traktor.setFuelLevel(20);
+		Traktor.setRashod(8);
+		if (Traktor.move("x", 100)) {
+			System.out.println("Traktor Новые координаты теперь х= "
+					+ Traktor.getCoords()[0] + " y= " + Traktor.getCoords()[1]);
+		} else {
+			System.out.println("Traktor Не хватит топлива");
+		}
 
-{float x = 0.25f;
-double chisl = Math.acos((x-b)*(x+b));
-double znamen = Math.asin((x-a)*(x+a));
-double y = chisl / znamen;
-System.out.print("x = "+ x + "y =" + y);}
+		if (Traktor.move("y", 500)) {
+			System.out.println("Traktor Новые координаты теперь х= "
+					+ Traktor.getCoords()[0] + " y= " + Traktor.getCoords()[1]);
+		} else {
+			System.out.println("Traktor Не хватит топлива");
+		}
+		System.out.println("Traktor Проверим координаты х= " + Traktor.getCoords()[0]
+				+ " y= " + Traktor.getCoords()[1]);
 
-{float x = 0.26f;
-double chisl = Math.acos((x-b)*(x+b));
-double znamen = Math.asin((x-a)*(x+a));
-double y = chisl / znamen;
-System.out.print("x = "+ x + "y ="+ y);}
+		
+		Car Kombayn= new Car(3000, 1400, 1800,4,5,5);
+		Kombayn.setPosition(0, 0);
+		Kombayn.setFuelLevel(30);
+		Kombayn.setRashod(5);
+		if (Kombayn.move("y", 100)) {
+			System.out.println("Kombayn Новые координаты теперь х= "
+					+ Traktor.getCoords()[0] + " y= " + Traktor.getCoords()[1]);
+		} else {
+			System.out.println("Kombayn Не хватит топлива");
+		}
 
-{float x = 0.37f;
-double chisl = Math.acos((x-b)*(x+b));
-double znamen = Math.asin((x-a)*(x+a));
-double y = chisl / znamen;
-System.out.print("x =" + x + "y ="+ y);}
+		if (Traktor.move("y", 100)) {
+			System.out.println("Kombayn Новые координаты теперь х= "
+					+ Traktor.getCoords()[0] + " y= " + Traktor.getCoords()[1]);
+		} else {
+			System.out.println("Kombayn Не хватит топлива");
+		}
 
-{float x = 0.48f;
-double chisl = Math.acos((x-b)*(x+b));
-double znamen = Math.asin((x-a)*(x+a));
-double y = chisl / znamen;
-System.out.print("x =" + x + "y ="+ y);}
-
-{float x = 0.56f;
-double chisl = Math.acos((x-b)*(x+b));
-double znamen = Math.asin((x-a)*(x+a));
-double y = chisl / znamen;
-System.out.print("x =" + x + "y ="+ y);}
-
-Calc defaultConstr =new Calc(0.15,0.26,0.37,0.48,0.56);
-System.out.print("-----------------Решаем задачу B-----------------");
-
-defaultConstr.taskB();
-System.out.print("--------------Решаем задачу A--------------------");
-
-defaultConstr.taskA(0.15,0.26,0.37,0.48,0.56);
-System.out.print("---------------Решаем задачу А с перегруженным конструктором-----------");
-
-Calc nDefConstr = new Calc(0.15,0.26,0.37,0.48,0.56);
-nDefConstr.taskA();
-
-        }
+		
+	}
 }
-
-
-
-        
-
-    
-    
-    
