a7621cd04bb903fa55d0ce9b542ed9d99975e9f0
SAMSUNG <SAMSUNG@Kseniya>
2012-12-08 10:25:23 +0400

    Laboratornaya rabota №3 1ая версия (рабочая)

--- Panzer/src/panzer/HelloPanzer.java
+++ Panzer/src/panzer/HelloPanzer.java
@@ -1,13 +1,34 @@
-            /*
+package panzer;            
+/*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package panzer;
+
 
 /**
  *
  * @author SAMSUNG
  */
 public class HelloPanzer {
+public static void main (String [] args) {
+/*Создаем Т54 - СССР*/
+Panzer T54 = new Panzer (1000, 1000, 2400);
 
-    
+    T54.setPosition(0,0);
+    T54.setFuelLevel(400);
+    T54.setConsumption (150);
+    if (T54.move("x",100)){
+        System.out.println("T54 Новые координаты расположения х = "+
+        T54.getCoordinations()[0]+"y="+T54.getCoordinations()[1]);
+    }else{
+        System.out.println("T54 не хватит топлива");
+    }
+    if (T54.move("y", 300)){
+        System.out.println("T54 Новые координаты теперь х="+
+                T54.getCoordinations()[] + "y=" + T54.getCoordinations()[1]);
+    }else{
+        System.out.println("T54 не хватит топлива");    
+    }    
+    System.out.println("T54 проверим кооринаты х =" + T54.getCoordinations()[0]
+            + "y =" + T54.getCoordinations()[1]);
+}}
