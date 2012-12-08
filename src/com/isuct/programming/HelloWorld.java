fe5fa043510ebc8a1e9dc2c0fccde73a3de9b18e
Stud_5 <Stud_5@5_08.efdnew.isuct.ru>
2012-12-08 10:56:32 +0400

    Лабораторная работа 4

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -4,90 +4,29 @@ package com.isuct.programming;
 
 public class HelloWorld{
 
-      public static void main(String [] args){
-			Raketa BlackBrant = new Raketa(27000, 5000, 20000);
-		BlackBrant.setPosition(1, 2);
-		BlackBrant.setFuelLevel(800);
-		BlackBrant.setRashod(50);
-                BlackBrant.setRezervtopliva(10);
-		if (BlackBrant.move("x", 100)) {
-			System.out.println("BlackBrant Новые координаты теперь х= "
-					+ BlackBrant.getCoords()[0] + " y= " + BlackBrant.getCoords()[1]);
-		} else {
-			System.out.println("BlackBrant Не хватит топлива");
+      public static void main(String[] args) {
+              		
+
+		Calc defaultConstr = new Calc(-2.5,3.4,3.5,0.6,6.5);
+		System.out.println("-Решаем задачу В-");
+		double taskB[][]=defaultConstr.taskB(new double[]{0.1,0.3,0.5,0.8,2.8,5.4,8});
+		for (int i=0;i<taskB[0].length;i++){
+			System.out.println("X= "+taskB[0][i]+" Y= "+ taskB[1][i]);
 		}
 
-		if (BlackBrant.move("y", 100)) {
-			System.out.println("BlackBrant Новые координаты теперь х= "
-					+ BlackBrant.getCoords()[0] + " y= " + BlackBrant.getCoords()[1]);
-		} else {
-			System.out.println("BlackBrant Не хватит топлива");
+		System.out.println("-Решаем задачу A -");
+		double taskA[][]=defaultConstr.taskA(-2.5,3.4,3.5,0.6,6.5);
+		for (int i=0;i<taskA[0].length;i++){
+			System.out.println("X= "+taskA[0][i]+" Y= "+ taskA[1][i]);
 		}
-		System.out.println("BlackBrant Проверим координаты х= " + BlackBrant.getCoords()[0]
-				+ " y= " + BlackBrant.getCoords()[1]);
-
-
-		Raketa Skylark= new Raketa(27000, 5000, 20000,0XFFFFFF,3,3);
-		Skylark.setPosition(1, 1);
-		Skylark.setFuelLevel(900);
-		Skylark.setRashod(90);
-                Skylark.setRezervtopliva(15);
-                
-		if (BlackBrant.move("y", 100)) {
-			System.out.println("Skylark Новые координаты теперь х= "
-					+ BlackBrant.getCoords()[0] + " y= " + BlackBrant.getCoords()[1]);
-		} else {
-			System.out.println("Skylark Не хватит топлива");
+		
+		System.out.println("-Решаем задачу A с перегруженным конструктором-");
+		Calc nDefConstr = new Calc(1.1,2.0,1,0.5,7);
+		//тогда мы можем вызвать метод без передачи дополнительных параметров
+		double taskA1[][]=nDefConstr.taskA(-2.5,3.4,3.5,0.6,6.5);
+		for (int i=0;i<taskA1[0].length;i++){
+			System.out.println("X= "+taskA1[0][i]+" Y= "+ taskA1[1][i]);
 		}
 
-		if (BlackBrant.move("y", 100)) {
-			System.out.println("Skylark Новые координаты теперь х= "
-					+ BlackBrant.getCoords()[0] + " y= " + BlackBrant.getCoords()[1]);
-		} else {
-			System.out.println("Skylark Не хватит топлива");
-		}         
-
-                if (BlackBrant.move("y",100)) {
-			System.out.println("Skylark Новые координаты теперь х= "
-					+ BlackBrant.getCoords()[0] + " y= " + BlackBrant.getCoords()[1]);
-		} else {
-			System.out.println("Skylark Не хватит топлива");
-		}
-              
-                
-                Raketa S= new Raketa(27000, 5000, 20000,0XFFFFFF,3,3);
-		Skylark.setPosition(1, 1);
-		Skylark.setFuelLevel(100);
-		Skylark.setRashod(10);
-                Skylark.setRezervtopliva(0);
-                
-		if (Skylark.move("y", 100)) {
-			System.out.println("S Новые координаты теперь х= "
-					+ Skylark.getCoords()[0] + " y= " + Skylark.getCoords()[1]);
-		} else {
-			System.out.println("S Не хватит топлива");
-		}
-
-                if (Skylark.move("y", 100)) {
-			System.out.println("S Новые координаты теперь х= "
-					+ Skylark.getCoords()[0] + " y= " + Skylark.getCoords()[1]);
-		} else {
-			System.out.println("S Не хватит топлива");
-		}
-                if (Skylark.move("y", 100)) {
-			System.out.println("S Новые координаты теперь х= "
-					+ Skylark.getCoords()[0] + " y= " + Skylark.getCoords()[1]);
-		} else {
-			System.out.println("S Не хватит топлива");
-		}
-
-                if (Skylark.move("y", 100)) {
-			System.out.println("S Новые координаты теперь х= "
-					+ Skylark.getCoords()[0] + " y= " + Skylark.getCoords()[1]);
-		} else {
-			System.out.println("S Не хватит топлива");
-		}
-                                
-            	}
+	}
 }
-    
