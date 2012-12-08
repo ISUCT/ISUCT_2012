595ed9014e16600be813cc1b8af3938e414d1522
Stud_5 <Stud_5@5_07.efdnew.isuct.ru>
2012-12-08 11:00:31 +0400

    Лабораторная работа №4

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -13,45 +13,25 @@ public class HelloWorld{
 	 /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-       Mouse SAMSUNG = new Mouse(1,1,5);
-       SAMSUNG.setPosition(0, 0);
-       SAMSUNG.setElektro(15);
-       
-       if (SAMSUNG.move("x", 100)) {
-			System.out.println("SAMSUNG Новые координаты теперь х= "
-					+ SAMSUNG.getCoords()[0] + " y= " + SAMSUNG.getCoords()[1]);
-		} else {
-			System.out.println("SAMSUNG Не хватит энергии");
-		}
-
-		if (SAMSUNG.move("y", 500)) {
-			System.out.println("SAMSUNG Новые координаты теперь х= "
-					+ SAMSUNG.getCoords()[0] + " y= " + SAMSUNG.getCoords()[1]);
-		} else {
-			System.out.println("SAMSUNG Не хватит энергии");
-		}
-		System.out.println("SAMSUNG Проверим координаты х= " + SAMSUNG.getCoords()[0]
-				+ " y= " + SAMSUNG.getCoords()[1]);
-                
-                Mouse TECH = new Mouse(1, 1, 5,2,3,0XFFFF);
-		TECH.setPosition(0, 0);
-		TECH.setElektro(20);	
-		if (SAMSUNG.move("y", 100)) {
-			System.out.println("TECH Новые координаты теперь х= "
-					+ SAMSUNG.getCoords()[0] + " y= " + SAMSUNG.getCoords()[1]);
-		} else {
-			System.out.println("TECH Не хватит энергии");
-		}
-
-		if (SAMSUNG.move("y", 100)) {
-			System.out.println("TECH Новые координаты теперь х= "
-					+ SAMSUNG.getCoords()[0] + " y= " + SAMSUNG.getCoords()[1]);
-		} else {
-			System.out.println("TECH Не хватит энергии");
-		}
-
-        
+   public static void main(String[] args) {
+        // TODO code application logic here
+    Calc defaultConstr = new Calc(2,0.95,1.25,0.3,2.75);
+    System.out.println("------------Решаем задачу В------------");
+    double taskB[][]=defaultConstr.taskB(new double[]{0.1,0.3,0.5,0.8,2.8,5.4,8});
+    for (int i=0;i<taskB[0].length;i++){
+    System.out.println("X= "+taskB[0][i]+" Y= "+ taskB[1][i]);
     }
-} 
-
+    System.out.println("------------Решаем задачу A ------------");
+    double taskA[][]=defaultConstr.taskA(2,0.95,1.25,0.3,2.75);
+    for (int i=0;i<taskA[0].length;i++){
+    System.out.println("X= "+taskA[0][i]+" Y= "+ taskA[1][i]);
+        }
+    System.out.println("------------Решаем задачу A с перегруженным конструктором------------");
+    Calc nDefConstr = new Calc(2,0.95,1.25,0.3,2.75);
+    double taskA1[][]=nDefConstr.taskA();
+    for (int i=0;i<taskA1[0].length;i++){
+    System.out.println("X= "+taskA1[0][i]+" Y= "+ taskA1[1][i]);
+    }
+	}
+}
+   
