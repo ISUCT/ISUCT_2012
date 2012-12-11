30a5114cc04052f4ef1a397be4b0d6a109718155
Алена <@>
2012-12-12 00:03:12 +0400

    Лабораторная работа №4. Двумерный массив.

--- src/com/isuct/programming/Calc.java
+++ src/com/isuct/programming/Calc.java
@@ -1,59 +1,103 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.isuct.programming;
 
-/**
- *
- * @author Алена
- */
-
 public class Calc {
-    
-        private double xn;
+	/**
+	 * Закрытые поля класса
+	 */
+	private double xn;
 	private double dx;
 	private double xk;
 	private double a;
-
 	
-	public Calc(){
+
+	/**
+	 * Конструктор по умолчанию
+	 */
+	public Calc() {
+
 	}
 
-	
+	/**
+	 * Конструктор, в котором можно передать параметры
+	 * @param aXn - начальное значение X
+	 * @param aDx - дельта
+	 * @param aXk - конечное значение Х
+	 */
 	public Calc(double aA, double aXn, double aDx, double aXk) {
+		// Устанавливаем значения закрытых полей
 		xn = aXn;
 		dx = aDx;
 		xk = aXk;
 		a = aA;
+		
 	}
-        
-	public void taskB() {
-            
+
+	/**
+	 * Решение задачи B
+	 */
+	public double[][] taskB(double[] arrayX) {
+		// Объявляем и инициализируем переменные
                 double a = 2.25f;
-		double y;
-		double[] Xarray = new double[] { 1.31,1.39,1.44,1.56,1.92 };
-		for (double x : Xarray) {			
+		double y = 0;
+		// Создаем 2мерный массив для хранения результатов
+		double[][] result = new double[2][arrayX.length];
+		int i = 0;
+		for (double x : arrayX) {
 			y = Math.pow(a, (x*x-1))-Math.log(x*x-1)/Math.log(10)+Math.pow((x*x-1), (1/3f));
-                        System.out.println("x="+x+"  y="+y);
-			
+			// Сохраняем результат
+			result[0][i] = x;
+			result[1][i] = y;
+			i++;
 		}
+		return result;
 	}
 
-	public void taskA() {
-		double y;
-		for (double x = xn; x <= xk; x += dx) {
-			 y = Math.pow(a, (x*x-1))-Math.log(x*x-1)/Math.log(10)+Math.pow((x*x-1), (1/3f));
-                         System.out.println("x="+x+"  y="+y);
-			
+	/**
+	 * Решение задачи А
+	 */
+	public double[][] taskA() {
+		// Объявляем и инициализируем переменные
+            
+		double y = 0;
+		//при создании вычисляем сколько будет элементов
+		int nElem = (int) Math.round((xk-xn)/dx+1);
+                
+		double[][] result = new double[2][nElem];
+		int i=0;
+		for (double x = xn; x < xk; x += dx) {
+			y = Math.pow(a, (x*x-1))-Math.log(x*x-1)/Math.log(10)+Math.pow((x*x-1), (1/3f));
+			// Выводим результат
+			result[0][i]=x;
+			result[1][i]=y;
+			i++;
 		}
+		return result;
 	}
 
-	public void taskA(double a, double xn, double dx, double xk) {	
-		double y;             
-                for (double x = xn; x <= xk; x += dx) {
-			 y = Math.pow(a, (x*x-1))-Math.log(x*x-1)/Math.log(10)+Math.pow((x*x-1), (1/3f));
-                         System.out.println("x="+x+"  y="+y);
+	/**
+	 * Перегруженный метод вычисления задачи, с возможностью
+	 * передачи параметров
+	 * @param xn
+	 * @param dx
+	 * @param xk
+	 * @param a
+	 */
+	public double[][] taskA(double a, double xn, double dx, double xk) {
+		// Объявляем и инициализируем переменные
+		double y = 0;
+		//при создании вычисляем сколько будет элементов
+		int nElem = (int) Math.round((xk-xn)/dx+1);
+		double[][] result = new double[2][nElem];
+		int i=0;
+                
+		for (double x = xn; x < xk; x += dx) {
+                        y = Math.pow(a, (x*x-1))-Math.log(x*x-1)/Math.log(10)+Math.pow((x*x-1), (1/3f));                       
+			// Выводим результат
+			result[0][i]=x;
+			result[1][i]=y;
+			i++;
 		}
+		return result;
 	}
+
 }
--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -1,22 +1,37 @@
 package com.isuct.programming;
 
 public class HelloWorld {
-    
-    public static void main(String[] args) {                     
-                       
-            Calc defaultConstr = new Calc();
-            
-            System.out.println("------------Решаем задачу В------------");
-            defaultConstr.taskB();
-            
-            System.out.println("------------Решаем задачу A —----------");
-            defaultConstr.taskA(2.25, 1.2, 0.3, 2.7);
-            
-            System.out.println("------------Решаем задачу A с перегруженным конструктором------------");
-            Calc nDefConstr = new Calc(2.25, 1.2, 0.3, 2.7);
-            nDefConstr.taskA();
-                      
-        }      
 
+	public static void main(String[] args) {
+		//Решим задачи А и задачу B с использованием внешнего класса Calc
+		//Сперва используем конструктор по умолчанию
+		//Теперь все методы внутри класса CALC нам просто возвращают массивы значений
+		//а их вывод мы реализуем здесь
+		//Это позволит нам в дальнейшем использовать класс Calc с графическим интерфейсом
+		//поскольку у нас теперь есть четкий "вход" и "выход" в каждом методе
+		//Свои "старые лабораторные" я получил отмотав назад в git
+
+		Calc defaultConstr = new Calc();
+		System.out.println("------------Решаем задачу В------------");
+		double taskB[][]=defaultConstr.taskB(new double[]{1.31,1.39,1.44,1.56,1.92 });
+		for (int i=0;i<taskB[0].length;i++){
+			System.out.println("X= "+taskB[0][i]+" Y= "+ taskB[1][i]);
+		}
+
+		System.out.println("------------Решаем задачу A ------------");
+		double taskA[][]=defaultConstr.taskA( 2.25, 1.2, 0.3, 2.7);
+		for (int i=0;i<taskA[0].length;i++){
+			System.out.println("X= "+taskA[0][i]+" Y= "+ taskA[1][i]);
+		}
+		//Решим задачу А - с использованием перегруженного конструктора
+		System.out.println("------------Решаем задачу A с перегруженным конструктором------------");
+		Calc nDefConstr = new Calc(2.25, 1.2, 0.3, 2.7);
+		//тогда мы можем вызвать метод без передачи дополнительных параметров
+		double taskA1[][]=nDefConstr.taskA();
+		for (int i=0;i<taskA1[0].length;i++){
+			System.out.println("X= "+taskA1[0][i]+" Y= "+ taskA1[1][i]);
+		}
+
+	}
 }
 
