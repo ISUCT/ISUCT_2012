579dfad708f1339071e3a009739250827d5a98ea
Admin <Admin@microsof-2bf323>
2012-12-14 01:00:32 +0300

    №4

--- src/com/isuct/programming/Calc.java
+++ src/com/isuct/programming/Calc.java
@@ -1,40 +1,99 @@
 package com.isuct.programming;
 
 public class Calc {
+	/**
+	 * Закрытые поля класса
+	 */
+	private double xn;
+	private double dx;
+	private double xk;
+	
 
-    private double xn;
-    private double dx;
-    private double xk; 
+	/**
+	 * Конструктор по умолчанию
+	 */
+	public Calc() {
 
-    /**
-     * Конструктор по умолчанию
-     */
-    public Calc( double aXn, double aDx, double aXk) {
-        xn = aXn;
-        dx = aDx;
-        xk = aXk;
-    }
-        public void taskB() {
-        double y = 0;
-        double[] Xarray = new double[]{1.0, 1.3, 1.6, 1.9, 2.1, 2.3, 7.1, 8.5, 9.9};
-        for (double x : Xarray) {
-            y =  (Math.pow(Math.sin(x),3)+(Math.pow(Math.cos(x),3))*Math.log10(x));
-                    System.out.println("x=" + x + "y=" + y);
-        }
-    }
+	}
 
-    public void taskA() {
-        double y = 0;
-        for (double x = 0.11; x <= 0.36; x = (x + 0.05)) {
-            y =  (Math.pow(Math.sin(x),3)+(Math.pow(Math.cos(x),3))*Math.log10(x));
-                    System.out.println("x=" + x + "y=" + y);
-    }
-    }
- public void taskA(double xn, double dx, double xk){
-  double y = 0;
-  for (double x = 0.11; x <= 0.36; x = (x + 0.05)) {
-            y =  (Math.pow(Math.sin(x),3)+(Math.pow(Math.cos(x),3))*Math.log10(x));
-                    System.out.println("x=" + x + "y=" + y);
-    }
- }       
+	/**
+	 * Конструктор, в котором можно передать параметры
+	 * @param aXn - начальное значение X
+	 * @param aDx - дельта
+	 * @param aXk - конечное значение Х
+	 */
+	public Calc(double aXn, double aDx, double aXk) {
+		// Устанавливаем значения закрытых полей
+		xn = aXn;
+		dx = aDx;
+		xk = aXk;
+		
+	}
+
+	/**
+	 * Решение задачи B
+	 */
+	public double[][] taskB(double[] arrayX) {
+		// Объявляем и инициализируем переменные
+		double y = 0;
+		// Создаем 2мерный массив для хранения результатов
+		double[][] result = new double[2][arrayX.length];
+		int i = 0;
+		for (double x : arrayX) {
+			y = (Math.pow(Math.sin(x),3)+(Math.pow(Math.cos(x),3))*Math.log10(x));
+			// Сохраняем результат
+			result[0][i] = x;
+			result[1][i] = y;
+			i++;
+		}
+		return result;
+	}
+
+	/**
+	 * Решение задачи А
+	 */
+	public double[][] taskA() {
+		// Объявляем и инициализируем переменные
+            
+		double y = 0;
+		//при создании вычисляем сколько будет элементов
+		int nElem = (int) Math.round((xk-xn)/dx+1);
+                
+		double[][] result = new double[2][nElem];
+		int i=0;
+		for (double x = xn; x < xk; x += dx) {
+			y = (Math.pow(Math.sin(x),3)+(Math.pow(Math.cos(x),3))*Math.log10(x));
+			// Выводим результат
+			result[0][i]=x;
+			result[1][i]=y;
+			i++;
+		}
+		return result;
+	}
+
+	/**
+	 * Перегруженный метод вычисления задачи, с возможностью
+	 * передачи параметров
+	 * @param xn
+	 * @param dx
+	 * @param xk
+	  */
+	public double[][] taskA(double xn, double dx, double xk) {
+		// Объявляем и инициализируем переменные
+		double y = 0;
+		//при создании вычисляем сколько будет элементов
+		int nElem = (int) Math.round((xk-xn)/dx+1);
+		double[][] result = new double[2][nElem];
+		int i=0;
+                
+		for (double x = xn; x < xk; x += dx) {
+                        y = (Math.pow(Math.sin(x),3)+(Math.pow(Math.cos(x),3))*Math.log10(x));                       
+			// Выводим результат
+			result[0][i]=x;
+			result[1][i]=y;
+			i++;
+		}
+		return result;
+	}
+
 }
--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -2,48 +2,35 @@ package com.isuct.programming;
 
 public class HelloWorld {
 
-public static void main(String[] args) {
-		// создаем собаку Барон
-		dog Baron = new dog(100, 20, 4, 500, 3);
-		Baron.setPosition(0, 0);
-		Baron.setSleepLevel(10);
-		Baron.setRashod(7);
-		if (Baron.move("x", 80)) {
-			System.out.println("Baron Новые координаты теперь х= "
-					+ Baron.getCoords()[0] + " y= " + Baron.getCoords()[1]);
-		} else {
-			System.out.println("Baron Не хватит уровня сна");
+	public static void main(String[] args) {
+		//Решим задачи А и задачу B с использованием внешнего класса Calc
+		//Сперва используем конструктор по умолчанию
+		//Теперь все методы внутри класса CALC нам просто возвращают массивы значений
+		//а их вывод мы реализуем здесь
+		//Это позволит нам в дальнейшем использовать класс Calc с графическим интерфейсом
+		//поскольку у нас теперь есть четкий "вход" и "выход" в каждом методе
+		//Свои "старые лабораторные" я получил отмотав назад в git
+
+		Calc defaultConstr = new Calc();
+		System.out.println("------------Решаем задачу В------------");
+		double taskB[][]=defaultConstr.taskB(new double[]{0.2,0.3,0.38,0.43,0.57 });
+		for (int i=0;i<taskB[0].length;i++){
+			System.out.println("X= "+taskB[0][i]+" Y= "+ taskB[1][i]);
 		}
 
-		if (Baron.move("y", 200)) {
-			System.out.println("Baron Новые координаты теперь х= "
-					+ Baron.getCoords()[0] + " y= " + Baron.getCoords()[1]);
-		} else {
-			System.out.println("Baron Не хватит уровня еды");
+		System.out.println("------------Решаем задачу A ------------");
+		double taskA[][]=defaultConstr.taskA(0.11, 0.05, 0.36);
+		for (int i=0;i<taskA[0].length;i++){
+			System.out.println("X= "+taskA[0][i]+" Y= "+ taskA[1][i]);
 		}
-		System.out.println("Baron Проверим координаты х= "
-				+ Baron.getCoords()[0] + " y= " + 
-Baron.getCoords()[1]);
-
-		// создаем собаку Чапа
-		dog Chapa = new dog(130, 15, 4, 400, 2);
-		Chapa.setPosition(0, 0);
-		Chapa.setSleepLevel(20);
-		Chapa.setRashod(9);
-
-		if (Chapa.move("y", 90)) {
-			System.out.println("Chapa Новые координаты теперь х= "
-					+Chapa.getCoords()[0] + " y= " + Chapa.getCoords()[1]);
-		} else {
-			System.out.println("Chapa Не хватит уровня сна");
+		//Решим задачу А - с использованием перегруженного конструктора
+		System.out.println("------------Решаем задачу A с перегруженным конструктором------------");
+		Calc nDefConstr = new Calc(0.11, 0.05, 0.36);
+		//тогда мы можем вызвать метод без передачи дополнительных параметров
+		double taskA1[][]=nDefConstr.taskA();
+		for (int i=0;i<taskA1[0].length;i++){
+			System.out.println("X= "+taskA1[0][i]+" Y= "+ taskA1[1][i]);
 		}
 
-		if (Chapa.move("y", 90)) {
-			System.out.println("Chapa Новые координаты теперь х= "
-					+ Chapa.getCoords()[0] + " y= " + Chapa.getCoords()[1]);
-		} else {
-			System.out.println("Chapa Не хватит уровня сна");
-		}
 	}
-
 }
