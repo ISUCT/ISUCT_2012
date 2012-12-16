f626fbca6aeeb11ec596c06d6099b63c565362ee
Nastya Lobanova <naastya95@gmail.com>
2012-12-16 11:01:35 +0600

    лаба 5

--- src/com/isuct/programming/Calc.java
+++ src/com/isuct/programming/Calc.java
@@ -3,6 +3,7 @@ package com.isuct.programming;
 /**
  * Выносим реализацию задач в отдельный класс
  * @author Nastya
+ *   
  */
 public class Calc {
 	/**
@@ -12,7 +13,7 @@ public class Calc {
 	private double dx;
 	private double xk;
 	private double a;
-	private double b;
+	
 
 	/**
 	 * Конструктор по умолчанию
@@ -27,13 +28,13 @@ public class Calc {
 	 * @param aDx - дельта
 	 * @param aXk - конечное значение Х
 	 */
-	public Calc(double aA, double aB, double aXn, double aDx, double aXk) {
+	public Calc(double aA,  double aXn, double aDx, double aXk) {
 		// Устанавливаем значения закрытых полей
 		xn = aXn;
 		dx = aDx;
 		xk = aXk;
 		a = aA;
-		b = aB;
+		
 	}
 
 	/**
@@ -41,42 +42,41 @@ public class Calc {
 	 */
 	public double[][] taskB(double[] arrayX) {
 		// Объявляем и инициализируем переменные
-	
+		
 		double y = 0;
 		// Создаем 2мерный массив для хранения результатов
 		double[][] result = new double[2][arrayX.length];
 		int i = 0;
 		for (double x : arrayX) {
 			if (x < 5) {
-				     for (x = 1.2f; x <= 3.7; x += 0.5) {
+				 for (x = 1.2f; x <= 3.7; x += 0.5) {
             y = (Math.pow((a), (x + 1) * (x - 1)) - (Math.log((x - 1) * (x + 1)) / Math.log(10)) + Math.pow(((x - 1) * (x + 1)), (1 / 3)));
-			}
 			// Сохраняем результат
 			result[0][i] = x;
 			result[1][i] = y;
 			i++;
 		}
-                       
+                        }
 		
-                }
+	}
                 return result;
         }
-	
-
 	/**
 	 * Решение задачи А
 	 */
 	public double[][] taskA() {
-		// Объявляем и инициализируем переменн
+		// Объявляем и инициализируем переменные
+		
 		double y = 0;
 		//при создании вычисляем сколько будет элементов
-		int nElem = (int) Math.round(((xk-xn)/dx+1));
+		int nElem = (int) Math.floor(((xk-xn)/dx+1));
 
-		double[][] result = new double[2][nElem];
+		double[][] result = new double[2][nElem+1];
 		int i=0;
-		for (double x = xn; x < xk; x += dx) {
+		for (double x = xn; x <= xk; x += dx) {
 			if (x < 5) {
-				     for (x = 1.2f; x <= 3.7; x += 0.5) {
+				// вычисляем числитель Math.pow(число, степень)
+				 for (x = 1.2f; x <= 3.7; x += 0.5) {
             y = (Math.pow((a), (x + 1) * (x - 1)) - (Math.log((x - 1) * (x + 1)) / Math.log(10)) + Math.pow(((x - 1) * (x + 1)), (1 / 3)));
 			}
 			// Выводим результат
@@ -86,8 +86,7 @@ public class Calc {
 		}
                 }
 		return result;
-        }
-	
+	}
 
 	/**
 	 * Перегруженный метод вычисления задачи, с возможностью
@@ -96,29 +95,29 @@ public class Calc {
 	 * @param dx
 	 * @param xk
 	 * @param a
-	 * @param b
+	
 	 */
-	public double[][] taskA(double a, double b, double xn, double dx, double xk) {
+	public double[][] taskA(double a, double xn, double dx, double xk) {
 		// Объявляем и инициализируем переменные
+		
 		double y = 0;
 		//при создании вычисляем сколько будет элементов
-		int nElem = (int) Math.round(((xk-xn)/dx+1));
-		double[][] result = new double[2][nElem];
+		int nElem = (int) Math.floor(((xk-xn)/dx+1));
+		double[][] result = new double[2][nElem+1];
 		int i=0;
 
-		for (double x = xn; x < xk; x += dx) {
+		for (double x = xn; x <= xk; x += dx) {
 			if (x < 5) {
-				     for (x = 1.2f; x <= 3.7; x += 0.5) {
+				 for (x = 1.2f; x <= 3.7; x += 0.5) {
             y = (Math.pow((a), (x + 1) * (x - 1)) - (Math.log((x - 1) * (x + 1)) / Math.log(10)) + Math.pow(((x - 1) * (x + 1)), (1 / 3)));
-			}
 			// Выводим результат
 			result[0][i]=x;
 			result[1][i]=y;
 			i++;
 		}
+                        }
                 }
 		return result;
-                }
+	}
         }
 
-	
