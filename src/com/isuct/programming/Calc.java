6cabf15c98d31ed998c992e2402581c73f41ebf3
User <User@home-da3ffb5371>
2012-12-16 16:23:06 +0400

    Лабораторная работа №3

--- src/com/isuct/programming/Calc.java
+++ src/com/isuct/programming/Calc.java
@@ -1,23 +1,28 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.isuct.programming;
 
-/**
- *
- * @author Stud_5
- */
+
 public class Calc {
+	/**
+	 * Закрытые поля класса
+	 */
 	private double xn;
 	private double dx;
 	private double xk;
 	private double a;
 	private double b;
-        
-        public Calc() {
+
+	
+	public Calc() {
+
 	}
-        public Calc(double aA, double aB, double aXn, double aDx, double aXk) {
+
+	/**
+	 * Конструктор, в котором можно передать параметры
+	 * @param aXn - начальное значение X
+	 * @param aDx - дельта
+	 * @param aXk - конечное значение Х
+	 */
+	public Calc(double aA, double aB, double aXn, double aDx, double aXk) {
 		// Устанавливаем значения закрытых полей
 		xn = aXn;
 		dx = aDx;
@@ -25,16 +30,20 @@ public class Calc {
 		a = aA;
 		b = aB;
 	}
-        public double[][] taskB(double[] arrayX) {
-		// Объявляем и инициализируем переменные
-	   double chisl = 0;
-	   double znamen = 0;
-	   double y = 0;
-		// Создаем 2мерный массив для хранения результатов
-	   double[][] result = new double[2][arrayX.length];
-	   int i = 0;
-	   for (double x : arrayX) {
-                if (x < 5) {
+
+	/**
+	 * Решение задачи B
+	 */
+	public double[][] taskB(double[] arrayX) {
+		
+		double chisl ;
+		double znamen ;
+		double y;
+		
+		double[][] result = new double[2][arrayX.length];
+		int i = 0;
+		for (double x : arrayX) {
+			if (x < 5) {
                     chisl =Math.pow(Math.log10(x), 2)*((Math.pow(a, 2))+x);
                     znamen = Math.sin(Math.pow(x, 2));
                     y = chisl / znamen;
@@ -43,68 +52,89 @@ public class Calc {
                     znamen =1.8+ Math.pow((Math.cos(a*x)),3);
                     y = chisl / znamen;
                     }  
-                result[0][i] = x;
-		result[1][i] = y;
-		i++;
-                   }
-           return result;
-                }
-        
-        
-          public double[][] taskA() {
-            
-              double chisl = 0;
-	      double znamen = 0;
-	      double y = 0;
-                
-              int nElem = (int) Math.round(((xk-xn)/dx+1));
-              double[][] result = new double[2][nElem];
-	      int i=0;
-		
-              for (double x = xn; x < xk; x += dx) {
-                   if (x < 5) {
-                      chisl =Math.pow(Math.log10(x), 2)*((Math.pow(a, 2))+x);
-                      znamen = Math.sin(Math.pow(x, 2));
-                      y = chisl / znamen;
-                   } else {
-                      chisl=Math.pow((a+(b*x)), 2.5);
-                      znamen =1.8+ Math.pow((Math.cos(a*x)),3);
-                      y = chisl / znamen;
-                  }
-                   result[0][i]=x;
-		   result[1][i]=y;
-		   i++;
+			
+			result[0][i] = x;
+			result[1][i] = y;
+			i++;
 		}
-	       return result;
+		return result;
 	}
-                
-                
-               public double[][] taskA(double a, double b, double xn, double dx, double xk) {
-                    double chisl = 0;
-		    double znamen = 0;
-		    double y = 0;
-                    
-                    int nElem = (int) Math.round(((xk-xn)/dx+1));
-		    double[][] result = new double[2][nElem];
-		    int i=0;
-		    for (double x = xn; x < xk; x += dx) {
-                    
-                    if (x < 5) {
-                       chisl =Math.pow(Math.log10(x), 2)*((Math.pow(a, 2))+x);
-                       znamen = Math.sin(Math.pow(x, 2));
-                       y = chisl / znamen;
-                    } else {
-                       chisl=Math.pow((a+(b*x)), 2.5);
-                       znamen =1.8+ Math.pow((Math.cos(a*x)),3);
-                       y = chisl / znamen;
-                         }
-               result[0][i]=x;
-	       result[1][i]=y;
-	       i++;
-                        }
-	     return result;
+
+	/**
+	 * Решение задачи А
+	 */
+	public double[][] taskA() {
+		
+		double chisl ;
+		double znamen ;
+		double y ;
+		
+		int nElem = (int) Math.floor(((xk-xn)/dx));
+
+		double[][] result = new double[2][nElem+1];
+		int i=0;
+		for (double x = xn; x <= xk; x += dx) {
+			if (x < 5) {
+                    chisl =Math.pow(Math.log10(x), 2)*((Math.pow(a, 2))+x);
+                    znamen = Math.sin(Math.pow(x, 2));
+                    y = chisl / znamen;
+                } else {
+                    chisl=Math.pow((a+(b*x)), 2.5);
+                    znamen =1.8+ Math.pow((Math.cos(a*x)),3);
+                    y = chisl / znamen;
+                    }  
+			// Выводим результат
+			result[0][i]=x;
+			result[1][i]=y;
+			i++;
+		}
+		return result;
 	}
-                }
+
+	/**
+	 * Перегруженный метод вычисления задачи, с возможностью
+	 * передачи параметров
+	 * @param xn
+	 * @param dx
+	 * @param xk
+	 * @param a
+	 * @param b
+	 */
+	public double[][] taskA(double a, double b, double xn, double dx, double xk) {
+		
+		double chisl ;
+		double znamen;
+		double y ;
+		
+		int nElem = (int) Math.floor(((xk-xn)/dx));
+		double[][] result = new double[2][nElem+1];
+		int i=0;
+
+		for (double x = xn; x <= xk; x += dx) {
+			if (x < 5) {
+                    chisl =Math.pow(Math.log10(x), 2)*((Math.pow(a, 2))+x);
+                    znamen = Math.sin(Math.pow(x, 2));
+                    y = chisl / znamen;
+                } else {
+                    chisl=Math.pow((a+(b*x)), 2.5);
+                    znamen =1.8+ Math.pow((Math.cos(a*x)),3);
+                    y = chisl / znamen;
+                    }  
+			
+			result[0][i]=x;
+			result[1][i]=y;
+			i++;
+		}
+		return result;
+	}
+
+}
+
+
+
+
+
+
 
 
 
