ecbf5c38d7f5dc770146dbc61237804e8bc14a52
Nastya Lobanova <naastya95@gmail.com>
2012-12-16 10:24:44 +0600

    лаба 4

--- src/com/isuct/programming/Calc.java
+++ src/com/isuct/programming/Calc.java
@@ -1,57 +1,124 @@
 package com.isuct.programming;
 
-public class Calc {
-
-private double xn;
-private double dx;
-private double xk;
-private double a;
-private double b;
 /**
+ * Выносим реализацию задач в отдельный класс
+ * @author Nastya
+ */
+public class Calc {
+	/**
+	 * Закрытые поля класса
+	 */
+	private double xn;
+	private double dx;
+	private double xk;
+	private double a;
+	private double b;
 
-* Конструктор по умолчанию
+	/**
+	 * Конструктор по умолчанию
+	 */
+	public Calc() {
 
-*/
-public Calc(){
-    /**
-     * Конструктор с параметрами
-     */  
-}
-public Calc(double aA, double aB, double aXn, double aDx, double aXk) {
-xn = aXn;
-dx = aDx;
-xk = aXk;
-a = aA;
-b = aB;
+	}
 
-}
-public void taskB() {
-double a=1.6f;
-double y = 0;
-double[] Xarray = new double[]{1.28, 1.36, 2.47, 3.6, 3.16};
-for (double x : Xarray) {
-y= (Math.pow ((a),(x+1)*(x-1))- (Math.log((x-1)*(x+1))/Math.log(10)) + Math.pow (((x-1)*(x+1)),(1/3)));
-System.out.println("Икс равен= " + x + "Игрек равен = " + y);
+	/**
+	 * Конструктор, в котором можно передать параметры
+	 * @param aXn - начальное значение X
+	 * @param aDx - дельта
+	 * @param aXk - конечное значение Х
+	 */
+	public Calc(double aA, double aB, double aXn, double aDx, double aXk) {
+		// Устанавливаем значения закрытых полей
+		xn = aXn;
+		dx = aDx;
+		xk = aXk;
+		a = aA;
+		b = aB;
+	}
 
-}
-}
+	/**
+	 * Решение задачи B
+	 */
+	public double[][] taskB(double[] arrayX) {
+		// Объявляем и инициализируем переменные
+	
+		double y = 0;
+		// Создаем 2мерный массив для хранения результатов
+		double[][] result = new double[2][arrayX.length];
+		int i = 0;
+		for (double x : arrayX) {
+			if (x < 5) {
+				     for (x = 1.2f; x <= 3.7; x += 0.5) {
+            y = (Math.pow((a), (x + 1) * (x - 1)) - (Math.log((x - 1) * (x + 1)) / Math.log(10)) + Math.pow(((x - 1) * (x + 1)), (1 / 3)));
+			}
+			// Сохраняем результат
+			result[0][i] = x;
+			result[1][i] = y;
+			i++;
+		}
+                       
+		
+                }
+                return result;
+        }
+	
 
+	/**
+	 * Решение задачи А
+	 */
+	public double[][] taskA() {
+		// Объявляем и инициализируем переменн
+		double y = 0;
+		//при создании вычисляем сколько будет элементов
+		int nElem = (int) Math.round(((xk-xn)/dx+1));
 
+		double[][] result = new double[2][nElem];
+		int i=0;
+		for (double x = xn; x < xk; x += dx) {
+			if (x < 5) {
+				     for (x = 1.2f; x <= 3.7; x += 0.5) {
+            y = (Math.pow((a), (x + 1) * (x - 1)) - (Math.log((x - 1) * (x + 1)) / Math.log(10)) + Math.pow(((x - 1) * (x + 1)), (1 / 3)));
+			}
+			// Выводим результат
+			result[0][i]=x;
+			result[1][i]=y;
+			i++;
+		}
+                }
+		return result;
+        }
+	
 
-public void taskA() {
-double y = 0;
- for (double x = xn; x <= xk; x += dx)  {
-y= (Math.pow ((a),(x+1)*(x-1))- (Math.log((x-1)*(x+1))/Math.log(10)) + Math.pow (((x-1)*(x+1)),(1/3)));
-System.out.println("Икс равен= " + x + "Игрек равен = " + y); 
-}
+	/**
+	 * Перегруженный метод вычисления задачи, с возможностью
+	 * передачи параметров
+	 * @param xn
+	 * @param dx
+	 * @param xk
+	 * @param a
+	 * @param b
+	 */
+	public double[][] taskA(double a, double b, double xn, double dx, double xk) {
+		// Объявляем и инициализируем переменные
+		double y = 0;
+		//при создании вычисляем сколько будет элементов
+		int nElem = (int) Math.round(((xk-xn)/dx+1));
+		double[][] result = new double[2][nElem];
+		int i=0;
 
-}
+		for (double x = xn; x < xk; x += dx) {
+			if (x < 5) {
+				     for (x = 1.2f; x <= 3.7; x += 0.5) {
+            y = (Math.pow((a), (x + 1) * (x - 1)) - (Math.log((x - 1) * (x + 1)) / Math.log(10)) + Math.pow(((x - 1) * (x + 1)), (1 / 3)));
+			}
+			// Выводим результат
+			result[0][i]=x;
+			result[1][i]=y;
+			i++;
+		}
+                }
+		return result;
+                }
+        }
 
-public void taskA(double a, double b, double xn, double dx, double xk){ 
-double y = 0;
-for (double x = xn; x <= xk; x += dx) {
-y= (Math.pow ((a),(x+1)*(x-1))- (Math.log((x-1)*(x+1))/Math.log(10)) + Math.pow (((x-1)*(x+1)),(1/3)));
-System.out.println("Икс равен= " + x + "Игрек равен = " + y);
-}
-}
-}
+	
