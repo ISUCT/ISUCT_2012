a1537e37b18f93fda81f13af1611ea07c2c47ca5
пользователь <@10.230.2.139>
2012-12-27 01:15:04 +0400

    lab1

---
+++ rrest/src/rrest/Rrest.java
@@ -0,0 +1,34 @@
+package rrest;
+
+public class Rrest;
+	public static void main(String[] args){
+float x = 0.08f;
+float a = 2.0f;
+float b = 3.0f;
+double y;
+y=Math.pow(Math.asin(x),a)+Math.pow(Math.acos(x),b);
+
+System.out.println("X = "+x+" Y = "+y);
+
+x=0.26f;
+y=Math.pow(Math.asin(x),a)+Math.pow(Math.acos(x),b);
+System.out.println("X = "+x+" Y = "+y);
+
+x=0.35f;
+y=Math.pow(Math.asin(x),a)+Math.pow(Math.acos(x),b);
+System.out.println("X = "+x+" Y = "+y);
+
+x=0.41f;
+y=Math.pow(Math.asin(x),a)+Math.pow(Math.acos(x),b);
+System.out.println("X = "+x+" Y = "+y);
+
+x=0.53f;
+y=Math.pow(Math.asin(x),a)+Math.pow(Math.acos(x),b);
+System.out.println("X = "+x+" Y = "+y);
+
+
+        }  
+
+}
+
+
