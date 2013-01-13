b922bebf82d63654643a4401e54be581f5dfa655
https://github.com/ISUCT/ISUCT_2012.git <>
2013-01-14 00:35:51 +0400

    работа с тестами

--- HelloWorld/src/helloworld/MainGui.java
+++ HelloWorld/src/helloworld/MainGui.java
@@ -4,11 +4,9 @@
  */
 package helloworld;
 
-/**
- *
- * @author user
- */
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -17,11 +15,11 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JTextPane;
 
+import com.isuct.programming.CalculateEngine;
 
-public class MainGui {
+
+public class MainGui extends JFrame{
 	//Объявление всех компонентов, которые нам нужны
-
-
 
 	//Метки для отображения информации
 	//Их сраз и инициализируем текстом
@@ -29,7 +27,7 @@ public class MainGui {
 	JLabel lblDx = new JLabel("Dx = ");
 	JLabel lblXk = new JLabel("Xk = ");
 	JLabel lblA = new JLabel("a = ");
-
+	JLabel lblB = new JLabel("b = ");
 
 	JLabel lblResult = new JLabel("Результат");
 
@@ -46,41 +44,43 @@ public class MainGui {
 
 	JPanel windowContent;
 	JFrame frame;
+	private final JPanel panel = new JPanel();
+	private final JPanel panel_1 = new JPanel();
 	// В конструкторе создаются все компоненты
 	// и добавляются на фрейм с помощью комбинации
 	// Borderlayout и Gridlayout
 	public MainGui() {
 		windowContent = new JPanel();
-		// Задаём схему для этой панели
-		GridLayout bl = new GridLayout(7,2);
-		windowContent.setLayout(bl);
-		//Размещаем компоненты на панель
+		windowContent.setLayout(new BorderLayout(0, 0));
 
-		windowContent.add(lblA);
-		windowContent.add(txtA);
-
-
-		windowContent.add(lblXn);
-		windowContent.add(txtXn);
-		windowContent.add(lblDx);
-		windowContent.add(txtDx);
-		windowContent.add(lblXk);
-		windowContent.add(txtXk);
-		windowContent.add(btnCalc);
-		windowContent.add(new JLabel());
-		windowContent.add(lblResult);
-
-		JScrollPane scrollPane = new JScrollPane(txtResult);
-		windowContent.add(scrollPane);
+		windowContent.add(panel_1, BorderLayout.WEST);
+		panel_1.setLayout(new GridLayout(0, 2, 0, 0));
+		panel_1.add(lblA);
+		panel_1.add(txtA);
+		panel_1.add(lblB);
+		panel_1.add(txtB);
+		panel_1.add(lblXn);
+		panel_1.add(txtXn);
+		panel_1.add(lblDx);
+		panel_1.add(txtDx);
+		panel_1.add(lblXk);
+		panel_1.add(txtXk);
+		windowContent.add(btnCalc, BorderLayout.SOUTH);
 
 		btnCalc.addActionListener(new CalculateEngine(this));
 
 		frame = new JFrame("Calculator");
 		frame.setContentPane(windowContent);
+
+		windowContent.add(panel, BorderLayout.CENTER);
+		panel.add(lblResult);
+
+		JScrollPane scrollPane = new JScrollPane(txtResult);
+		panel.add(scrollPane);
 		// делаем размер окна достаточным
 		// для того, чтобы вместить все компоненты
 		frame.pack();
-		// Наконец, отображаем окно
+		// отображаем окно
 		frame.setVisible(true);
 
 	}
