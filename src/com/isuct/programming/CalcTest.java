4b60ea075c540262d965706eb4edcdc96f999e6f
Алена <@>
2013-01-14 19:04:11 +0400

    Лабораторная работа с тестами.

--- src/com/isuct/programming/CalcTest.java
+++ src/com/isuct/programming/CalcTest.java
@@ -50,7 +50,7 @@ public class CalcTest {
 		// Как Х передали 1, значит и в результате Х должен быть 1
 		assertEquals(7,res[0][0], 0.001);
 		// теперь Y проверим
-		// assertEquals(res[1], 7);
+		assertEquals(18.135, res[1][0],0.001);
 	}
 
 	@Test
