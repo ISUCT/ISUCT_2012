18ee62b7a2094500b0fcb3df4c071b9b70ca624f
пользователь <@10.230.2.139>
2013-01-15 15:27:28 +0400

    Лабораторная работа 4 (2) арзу

---
+++ Hello/src/hello/Hello.java
@@ -0,0 +1,11 @@
+package Hello;
+
+import Fox;
+public class Hello{
+
+public static void main(String[] args) {
+
+Fox D = new Fox(60, "pug", 40, 20, "orange", "Lis", 5, 4);
+System.out.println(D.getBreed());
+}
+}
