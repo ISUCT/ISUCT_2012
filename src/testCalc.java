fde2a045861510019c57441021a3aaf437c7bd2c
Admin <Admin@microsof-2bf323>
2013-01-16 00:57:56 +0300

    тесты

---
+++ src/testCalc.java
@@ -0,0 +1,90 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Admin
+ */
+public class testCalc {
+    
+@Test
+	public void testCalc() {
+		// тестируем конструктор (оба варианта)
+		Calc simpleConstr = new Calc();
+		Calc defConstr = new Calc(0.11,0.36);
+		if (simpleConstr != null & defConstr != null) {
+			assertTrue(true);
+		} else {
+			fail();
+		}
+	}
+
+	@Test
+	public void testTaskB0() {
+		Calc forTest = new Calc();
+		double[][] res = forTest.taskB(new double[] { 0 });
+		assertEquals(0,res[0][0], 0.001);
+		assertEquals(Double.NaN,res[1][0], 0.001);
+	}
+
+	@Test
+	public void testTaskB1() {
+		Calc forTest = new Calc();
+		double[][] res = forTest.taskB(new double[] { 1 });
+		assertEquals(1,res[0][0], 0.001);
+		assertEquals(1.188,res[1][0], 0.001);
+	}
+
+	@Test
+	public void testTaskB7() {
+		Calc forTest = new Calc();
+		double[][] res = forTest.taskB(new double[] { 7 });
+		assertEquals(7,res[0][0], 0.001);
+
+	}
+
+	@Test
+	public void testTaskA() {
+
+		Calc forTestParam = new Calc(0.11,0.36);
+		double[][] res = forTestParam.taskA();
+
+		assertEquals(0, res[0][0], 0.001);
+		assertEquals(5, res[0][5], 0.001);
+		assertEquals(9, res[0][9], 0.001);
+		assertEquals(10, res[0][res[0].length - 1], 0.001);
+
+		assertTrue(Double.isInfinite(res[1][0]));
+		assertEquals(10.374, res[1][5], 0.001);
+		assertEquals(25.031, res[1][9], 0.001);
+		assertEquals(29.549, res[1][res[0].length - 1], 0.001);
+
+
+		Calc forTestNoParam = new Calc();
+		try {
+			res = forTestNoParam.taskA();
+			fail(); 
+		} catch (ArrayIndexOutOfBoundsException arrEx) {
+			assertTrue(true);
+		}
+	}
+
+	@Test
+	public void testTaskADoubleDoubleDoubleDoubleDouble() {
+
+		Calc forTestParam = new Calc();
+		double[][] res = forTestParam.taskA(0.11,0.36);
+
+		assertEquals(0, res[0][0], 0.001);
+		assertEquals(5, res[0][5], 0.001);
+		assertEquals(9, res[0][9], 0.001);
+		assertEquals(10, res[0][res[0].length - 1], 0.001);
+
+		assertTrue(Double.isInfinite(res[1][0]));
+		assertEquals(10.374, res[1][5], 0.001);
+		assertEquals(25.031, res[1][9], 0.001);
+		assertEquals(29.549, res[1][res[0].length - 1], 0.001);
+	}
+}
