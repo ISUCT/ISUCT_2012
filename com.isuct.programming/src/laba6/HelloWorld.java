dac6aded79c8e3771df90d47cdcde7776136a2e6
Пользователь <@10.26.31.29>
2013-01-18 13:18:44 +0400

    Лабораторная с графическим интерфейсом

---
+++ com.isuct.programming/src/laba6/HelloWorld.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laba6;
+
+/**
+ *
+ * @author Пользователь
+ */
+
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+	
+    
+}
+}
