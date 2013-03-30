297de4791b38e350d68ef49812d7fd708d53f715
User <User@home-da3ffb5371>
2013-03-30 21:08:00 +0400

    Регулярное выражение(Геликс)

---
+++ Test/src/TestRegEx.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+/**
+ *
+ * @author home
+ */
+public class TestRegEx {
+
+    /**
+     * @param args the command line arguments
+     */
+
+private static String REGEX= "\\$(\\D{5}),(\\d{6}),(\\D{1}),([0-9]{4}.[0-9]{4}),([NS]{1}),([0-9]{4}.[0-9]{4}),([WE]{1}),([0-9]{1}.[0-9]{1}),([0-9]{3}.[0-9]{1}),([0-9]{6}),([WE]*),\\*([0-9]{2})*";
+
+private Pattern myPattern=Pattern.compile(REGEX);
+public boolean checkNum(String aNum){
+
+Matcher myMatch = myPattern.matcher(aNum); 
+String result="tel. ";
+if(myMatch.find()){
+for (int i=1;i<=myMatch.groupCount();i++){
+//Выводим группы номеров, 0 группа - это вся строка - выводить её не будем
+//поэтому номер и начинается с 1
+System.out.println(myMatch.group(i));
+//группы объеденим в результтрующйю строку и выведем её 
+//Для примера такое действие сгодиться
+result+=myMatch.group(i);
+}
+System.out.println(result);
+
+
+return true;
+}else{
+return false;
+}
+
+}
+}
