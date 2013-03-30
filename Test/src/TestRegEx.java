e2fb4b6e194cd67018a2f8140ae3276156a98d5f
User <User@home-da3ffb5371>
2013-03-30 21:42:30 +0400

    Регулярное выражение(Геликс)

---
+++ Test/src/TestRegEx.java
@@ -0,0 +1,34 @@
+
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+
+public class TestRegEx {
+
+    /**
+     * @param args the command line arguments
+     */
+
+private static String REGEX= "\\$(\\D{5}),(\\d{6}),(\\D{1}),([0-9]{4}.[0-9]{4}),([NS]{1}),([0-9]{4}.[0-9]{4}),([WE]{1}),([0-9]{1}.[0-9]{1}),([0-9]{3}.[0-9]{1}),([0-9]{6}),([WE]*),\\*([0-9]{2})*";
+
+private Pattern myPattern=Pattern.compile(REGEX);
+public boolean checkNum(String aNum){
+
+Matcher myMatch = myPattern.matcher(aNum); 
+String result="tel. ";
+if(myMatch.find()){
+for (int i=1;i<=myMatch.groupCount();i++){
+System.out.println(myMatch.group(i));
+result+=myMatch.group(i);
+}
+System.out.println(result);
+
+
+return true;
+}else{
+return false;
+}
+
+}
+}
+
