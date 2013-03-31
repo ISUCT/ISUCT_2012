c9c5797e31800c9edf00a6b117a73396156ffe0b
User <User@home-da3ffb5371>
2013-03-31 17:18:44 +0400

    Регулярное выражение(Геликс)

--- Test/test/TestRegExTest.java
+++ Test/test/TestRegExTest.java
@@ -1,23 +1,20 @@
 
-
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-
-public class TestRegExTestRegEx {
+public class TestRegExTest {
     
-    public TestRegExTestRegEx() {
+    public TestRegExTest() {
     }
+
     @Test
     public void testCheckNum() {
-        System.out.println("checkNum");
+          System.out.println("checkNum");
         String aNum = "$GPRMC,110457,A,5552.1776,N,3732.6664,E,0.1,147.6,210408,,*26";
         TestRegEx instance = new TestRegEx();
         boolean expResult = false;
         boolean result = instance.checkNum(aNum);
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-//        fail("The test case is a prototype.");
+
     }
 }
-
