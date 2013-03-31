0c3d2103137842f568a1eba66203af1f16bfdda5
User <User@home-da3ffb5371>
2013-03-31 21:20:40 +0400

    Правильный вариант регулярного выражения(Геликс)

--- Test/src/TestRegEx.java
+++ Test/src/TestRegEx.java
@@ -4,7 +4,7 @@ import java.util.regex.Matcher;
 
 public class TestRegEx {
     
-private static String REGEX= "\\$(\\D{5}),(\\d{6}),(\\D{1}),([0-9]{4}.[0-9]{4}),([NS]{1}),([0-9]{4}.[0-9]{4}),([WE]{1}),([0-9]{1}.[0-9]{1}),([0-9]{3}.[0-9]{1}),([0-9]{6}),(\\D{2}),\\*([0-9]{2})*";
+private static String REGEX= "\\$(\\D{5}),(\\d{6}),(\\D{1}),([0-9]{4}.[0-9]{4}),([NS]{1}),([0-9]{4}.[0-9]{4}),([WE]{1}),([0-9]{1}.[0-9]{1}),([0-9]{3}.[0-9]{1}),([0-9]{6}),([EW].*),\\*([0-9]{2})*";
 private Pattern myPattern=Pattern.compile(REGEX);
 public boolean checkNum(String aNum){
 
