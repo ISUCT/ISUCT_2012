9c8ecef09ab5325a2b7eacbc4000702e570bb0f6
Stud_17 <Stud_17@17_14.efdnew.isuct.ru>
2013-04-09 12:18:46 +0400

    пример выполнения лабораторной работы

---
+++ JavaApplication1/src/TestRegEx.java
@@ -0,0 +1,32 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Stud_17
+ */
+public class TestRegEx {
+    
+private static String REGEX= "\\$(\\D{5}),(\\d{6}),(\\D{1}),([0-9]{4}.[0-9]{4}),([NS]{1}),([0-9]{4}.[0-9]{4}),([WE]{1}),([0-9]{1}.[0-9]{1}),([0-9]{3}.[0-9]{1}),([0-9]{6}),([EW].*),\\*([0-9]{2})*";
+private Pattern myPattern=Pattern.compile(REGEX);
+public boolean checkNum(String aNum){
+
+Matcher myMatch = myPattern.matcher(aNum); 
+String result="tel. ";
+if(myMatch.find()){
+for (int i=1;i<=myMatch.groupCount();i++){
+System.out.println(myMatch.group(i));
+result+=myMatch.group(i);
+}
+System.out.println(result);
+
+
+return true;
+}else{
+return false;
+}
+
+}
+}
---
+++ JavaApplication1/src/TestRegExTest.java
@@ -0,0 +1,25 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Stud_17
+ */
+public class TestRegExTest {
+    
+     public TestRegExTest() {
+    }
+
+    @Test
+    public void testCheckNum() {
+          System.out.println("checkNum");
+        String aNum = "$GPRMC,110457,A,5552.1776,N,3732.6664,E,0.1,147.6,210408,,*26";
+        TestRegEx instance = new TestRegEx();
+        boolean expResult = false;
+        boolean result = instance.checkNum(aNum);
+        assertEquals(expResult, result);
+        
+    }
+}
