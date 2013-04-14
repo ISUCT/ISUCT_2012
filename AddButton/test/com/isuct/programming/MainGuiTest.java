068bffdb8ec87c4b344e610ba5ac3e2e930588cf
Пользователь <@OEM>
2013-04-14 17:31:14 +0400

    Сделал кнопки(теперь считает) + тест

--- AddButton/test/com/isuct/programming/MainGuiTest.java
+++ AddButton/test/com/isuct/programming/MainGuiTest.java
@@ -4,6 +4,12 @@
  */
 package com.isuct.programming;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import org.junit.After;
 import org.junit.Test;
 import static org.junit.Assert.*;
@@ -13,6 +19,8 @@ import static org.junit.Assert.*;
  * @author Пользователь
  */
 public class MainGuiTest {
+    String fName="test.txt";
+
     
     public MainGuiTest() {
     }
@@ -22,6 +30,35 @@ public class MainGuiTest {
     }
 
     @Test
-    public void testSomeMethod() {
-    }
+    public void testWrite() {
+String tst="Simple string";
+File file = new File(fName);
+try{
+	PrintWriter out = new PrintWriter(file);
+	out.println(tst);
+	out.close();
+	assertTrue(true);
+}catch(FileNotFoundException fnfEx){
+fail("Something bad");
+}
+
+
+}
+
+
+@Test
+public void testRead() {
+File file = new File(fName);
+try{
+BufferedReader br = new BufferedReader(new FileReader(file));
+String s;
+while((s=br.readLine())!=null){
+System.out.print(s);
+}
+br.close();
+assertTrue(true);
+}catch(IOException fnfEx){
+fail("Something bad");
+}
+}
 }
