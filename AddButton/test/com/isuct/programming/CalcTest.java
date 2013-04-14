b7ca143c7e64868fa38f36895403e30967fb2a7c
Пользователь <@OEM>
2013-04-14 17:09:34 +0400

    Сделал кнопки(теперь считает)

---
+++ AddButton/test/com/isuct/programming/CalcTest.java
@@ -0,0 +1,35 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.isuct.programming;
+
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Пользователь
+ */
+public class CalcTest {
+    
+    public CalcTest() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testTaskB() {
+    }
+
+    @Test
+    public void testTaskA_0args() {
+    }
+
+    @Test
+    public void testTaskA_4args() {
+    }
+}
---
+++ AddButton/test/com/isuct/programming/CalculateEngineTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.isuct.programming;
+
+import org.junit.After;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Пользователь
+ */
+public class CalculateEngineTest {
+    
+    public CalculateEngineTest() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void testActionPerformed() {
+    }
+}
