d1c103b7ea6f6dc6778e3e90ac657ba0a7774d3b
home <home@192.168.0.74>
2013-04-16 20:26:38 +0400

    Сохранение в файл и чтение из файла ( конечный вариант)

--- src/com/isuct/programming/HelloWorld.java
+++ src/com/isuct/programming/HelloWorld.java
@@ -1,11 +1,14 @@
+
 package com.isuct.programming;
 
+import java.text.DecimalFormat;
 
 public class HelloWorld {
     
 
-    public static void main(String[] args) {
-	   MainGui myGui = new MainGui(); 
-
+	public static void main(String[] args) {
+		MainGui myGui = new MainGui(); 
+		
 	}
 }
+
