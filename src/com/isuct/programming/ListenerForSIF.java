e9ad4edf95eee8de4ee78ca2ca2603f41a7ae6b3
home <home@192.168.0.74>
2013-04-16 20:13:34 +0400

    Сохранение в файл и чтение из файла (IO) конечный вариант

--- src/com/isuct/programming/ListenerForSIF.java
+++ src/com/isuct/programming/ListenerForSIF.java
@@ -1,42 +1,39 @@
 
 package com.isuct.programming;
 
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 
-public  class ListenerForSIF implements ActionListener {
-  
-	MainGui parent;
+public class ListenerForSIF implements ActionListener {
+    
+    MainGui parent;
 
-	public ListenerForSIF(MainGui gui) {
-		
-		parent = gui;
-	}
-
-	String fName = "test.txt";
-
-	@Override
-	public void actionPerformed(ActionEvent arg0) {
-	
+    public ListenerForSIF(MainGui gui) {
+   
+		parent = gui;   
+    }
+    
+    String fName = "test.txt";
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+   
 		System.out.println("Сохранение");
 
 		String textResult = parent.txtResult.getText();
 		File file = new File(fName);
-		try {
-			PrintWriter out = new PrintWriter(file);
-			out.println(textResult);
-			out.close();
-			assertTrue(true);
+                 	try {
+		PrintWriter out = new PrintWriter(file);
+		out.println(textResult);
+		out.close();
+		//assertTrue(true);
 		} catch (FileNotFoundException fnfEx) {
-			fail("Something bad");
+		//fail("Something bad");
 		}
-
-	}
-
+        
+     
+    }
 }
