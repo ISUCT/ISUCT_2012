a8fcb8cb73938729d4c672dd93a9e8a57abf53b0
home <home@192.168.0.74>
2013-05-20 16:55:15 +0400

    Client-Server

---
+++ Calc/src/calc/CS.java
@@ -0,0 +1,12 @@
+
+package calc;
+
+public class CS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        MainGui my = new MainGui();
+    }
+}
