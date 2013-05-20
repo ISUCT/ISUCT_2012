0ac7c42e0683ee4ae1e1f35b4c424e53268f887d
home <home@192.168.0.74>
2013-05-20 16:55:39 +0400

    Client-Server

---
+++ Calc/src/calc/IOButtonRead.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calc;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ *
+ * @author stud
+ */
+public class IOButtonRead implements ActionListener{
+    public IOButtonRead(MainGui gui) {
+        // TODO Auto-generated constructor stu   
+    }
+      String fName = "test.txt";  
+    @Override
+    public void actionPerformed(ActionEvent e) {
+     // TODO Auto-generated method stub
+        
+     System.out.println("Чтение");
+
+		File file = new File(fName);
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(file));
+			String s;
+			while ((s = br.readLine()) != null) {
+				System.out.print(s);
+			}
+			br.close();
+			//assertTrue(true);
+		} catch (IOException fnfEx) {
+			//fail("Something bad");
+                
+                }   
+        
+    }
+    
+}
