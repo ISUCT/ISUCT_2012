0caecec09095d09526b61c22ab032e3e9fa0ece8
Chernenkov_Maxim <maks.chernenkov@mail.ru>
2013-05-23 17:13:15 +0400

    1

--- src/com/isuct/programming/Server.java
+++ src/com/isuct/programming/Server.java
@@ -7,7 +7,7 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Serverok {
+public class Server {
 
 	public static void main(String[] args) throws IOException {
 
