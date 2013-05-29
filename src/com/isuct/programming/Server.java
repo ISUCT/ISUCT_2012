db5526d0640617a0cf6cd10491f5536d07748f48
MariaMoon <maria.krotkova@mail.ru>
2013-05-29 17:02:16 +0400

    Клиент-сервер

--- src/com/isuct/programming/Server.java
+++ src/com/isuct/programming/Server.java
@@ -8,45 +8,67 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
-	  public static void main(String[] args) throws IOException {
-		    System.out.println("Welcome to Server side");
-		    BufferedReader in = null;
-		    PrintWriter    out= null;
 
-		    ServerSocket servers = null;
-		    Socket       fromclient = null;
+	public static void main(String[] args) throws IOException {
 
-		    // create server socket
-		    try {
-		      servers = new ServerSocket(4444);
-		    } catch (IOException e) {
-		      System.out.println("Couldn't listen to port 4444");
-		      System.exit(-1);
-		    }
+		System.out.println("Welcome to Server side");
+		BufferedReader in = null;
+		PrintWriter out = null;
 
-		    try {
-		      System.out.print("Waiting for a client...");
-		      fromclient= servers.accept();
-		      System.out.println("Client connected");
-		    } catch (IOException e) {
-		      System.out.println("Can't accept");
-		      System.exit(-1);
-		    }
+		ServerSocket servers = null;
+		Socket fromclient = null;
 
-		    in  = new BufferedReader(new 
-		     InputStreamReader(fromclient.getInputStream()));
-		    out = new PrintWriter(fromclient.getOutputStream(),true);
-		    String input;
+		// create server socket
+		try {
+			servers = new ServerSocket(4444);
+		} catch (IOException e) {
+			System.out.println("Couldn't listen to port 4444");
+			System.exit(-1);
+		}
 
-		    System.out.println("Wait for messages");
-		    while ((input = in.readLine()) != null) {
-		     if (input.equalsIgnoreCase("exit")) break;
-		     out.println("You said >"+input);
-		     System.out.println(input);
-		    }
-		    out.close();
-		    in.close();
-		    fromclient.close();
-		    servers.close();
-		  }
+		try {
+			System.out.print("Waiting for a client...");
+			fromclient = servers.accept();
+			System.out.println("Client connected");
+		} catch (IOException e) {
+			System.out.println("Can't accept");
+			System.exit(-1);
+		}
+
+		in = new BufferedReader(new InputStreamReader(
+				fromclient.getInputStream()));
+		out = new PrintWriter(fromclient.getOutputStream(), true);
+		String input;
+
+		System.out.println("Wait for messages");
+		while ((input = in.readLine()) != null) {
+			if (input.equalsIgnoreCase("exit"))
+				break;
+						
+			String parts[] = input.split(",");
+
+			Calc defaultConstr = new Calc();
+			double a = Double.parseDouble(parts[0]);
+			double b = Double.parseDouble(parts[1]);
+			double xn = Double.parseDouble(parts[2]);
+			double dx = Double.parseDouble(parts[3]);
+			double xk = Double.parseDouble(parts[4]);
+			double taskA[][] = defaultConstr.taskA(a, b, xn, dx, xk);
+			String textResult = "";
+
+			for (int m = 0; m < taskA[0].length; m++) {
+				textResult += "X = " + taskA[0][m] + " Y= " + taskA[1][m]
+						+ "\r\n";
+			}
+
+			out.println("Result> " + textResult);
+			System.out.println(textResult);
+
+		}
+
+		out.close();
+		in.close();
+		fromclient.close();
+		servers.close();
+	}
 }
