1d6007ea1fe86ebd7727ac554b40226af692f299
hp <hp@128.73.41.65>
2013-06-05 14:09:35 +0400

    Лабораторная работа №4. Многопоточность

--- src/com/isuct/programming/Server.java
+++ src/com/isuct/programming/Server.java
@@ -7,7 +7,7 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server {
+public class Server     {
 
     public static void main(String[] args) throws IOException {
 
